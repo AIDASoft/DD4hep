b1f6e9751042b3a8199d38918fda37a0ad5047cc
Markus Frank <markus.frank@cern.ch>
2012-08-02 09:46:32 +0000

    Bug fix

--- DDExamples/CLICSiD/src/MultiLayerTracker_geo.cpp
+++ DDExamples/CLICSiD/src/MultiLayerTracker_geo.cpp
@@ -31,16 +31,16 @@ static Ref_t create_detector(LCDD& lcdd, const xml_h& e, SensitiveDetector& sens
     double  rmin = x_layer.inner_r();
     double  r    = rmin;
     int m = 0;
-      
+
     for(xml_coll_t j(x_layer,_X(slice)); j; ++j, ++m)  {
       xml_comp_t x_slice = j;
       Material mat = lcdd.material(x_slice.materialStr());
       string s_name= l_name+_toString(m,"_slice%d");
-      Tube   s_tub;
+      double thickness = x_slice.thickness();
+      Tube   s_tub(r,r+thickness,2*z,2*M_PI);
       Volume s_vol(s_name, s_tub, mat);
-        
-      r += x_slice.thickness();
-      s_tub.setDimensions(r,r,2*z,2*M_PI);
+
+      r += thickness;
       if ( x_slice.isSensitive() ) s_vol.setSensitiveDetector(sens);
       // Set Attributes
       s_vol.setAttributes(lcdd,x_slice.regionStr(),x_slice.limitsStr(),x_slice.visStr());
@@ -49,6 +49,7 @@ static Ref_t create_detector(LCDD& lcdd, const xml_h& e, SensitiveDetector& sens
       spv.addPhysVolID(_X(layer),n);
     }
     l_tub.setDimensions(rmin,r,z,2*M_PI);
+    cout << l_name << " " << rmin << " " << r << " " << z << endl;
     l_vol.setVisAttributes(lcdd,x_layer.visStr());
       
     PlacedVolume lpv = motherVol.placeVolume(l_vol,IdentityPos());
