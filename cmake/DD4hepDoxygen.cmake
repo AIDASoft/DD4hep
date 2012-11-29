54cd33208c4304b636bdf7b33d70a3b4a6226802
Pere Mato <Pere.Mato@cern.ch>
2012-11-29 11:32:17 +0000

    Added Doxygen documentation

---
+++ cmake/DD4hepDoxygen.cmake
@@ -0,0 +1,10 @@
+#---Add a target to generate API documentation with Doxygen-------------------------
+find_package(Doxygen)
+if(DOXYGEN_FOUND)
+  configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
+  add_custom_target(doc ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                    COMMENT "Generating API documentation with Doxygen" VERBATIM)
+else()
+  message(STATUS "Doxygen command not found. Documentation target (doc) not available")
+endif()
