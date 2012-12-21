99dc52a7442eba27bcbce6c4588e08fc39430e59
Frank Gaede <frank.gaede@desy.de>
2012-12-21 13:31:26 +0000

    - fixed code for creating the ILDExVXD geometry  - made parameters for ILDExVXD more ILD like

--- DDExamples/ILDExDet/src/compact/ILDExVXD_geo.cpp
+++ DDExamples/ILDExDet/src/compact/ILDExVXD_geo.cpp
@@ -23,10 +23,14 @@ static Ref_t create_element(LCDD& lcdd, const xml_h& e, SensitiveDetector& sens)
   vxd_data->id = x_det.id();
 
   Volume      mother = lcdd.pickMotherVolume(vxd);    
+
   for(xml_coll_t c(e,_X(layer)); c; ++c)  {
+
     xml_comp_t  x_layer  (c);
+
     xml_comp_t  x_support (x_layer.child(_X(support)));
     xml_comp_t  x_ladder  (x_layer.child(_X(ladder)));
+
     int         layer_id   = x_layer.id();
     int         nLadders   = x_ladder.number();
     string      layername  = name+_toString(layer_id,"_layer%d");
@@ -37,21 +41,27 @@ static Ref_t create_element(LCDD& lcdd, const xml_h& e, SensitiveDetector& sens)
     double      sens_thick = x_ladder.thickness();
     double      supp_thick = x_support.thickness();
     double      radius     = sens_radius + ((sens_thick+supp_thick)/2. - sens_thick/2.);
+    double      phi0       =  x_layer.phi0() ;
 
-    double      width      = 2.*tan(dphi/2.)*(sens_radius-sens_thick/2.);
-    Box         ladderbox ((sens_thick+supp_thick)/2.,width/2.,zhalf);
+    //    double      width      = 2.*tan(dphi/2.)*(sens_radius-sens_thick/2.);
+    double      width      =  x_ladder.width();
+
+    Box         ladderbox ( (sens_thick+supp_thick)/2., width/2., zhalf );
+
     Volume      laddervol (layername+"_ladder",ladderbox,lcdd.air());
 
     Material    sensmat    = lcdd.material(x_ladder.materialStr());
-    Box         sensbox   (sens_thick/2.,width/2.,zhalf);
+    Box         sensbox   (sens_thick/2.,  width/2.,   zhalf);
     Volume      sensvol   (layername+"_sens",sensbox,sensmat);
 
     Material    suppmat  = lcdd.material(x_support.materialStr());
     Box         suppbox   (supp_thick/2.,width/2.,zhalf);
     Volume      suppvol   (layername+"_supp",suppbox,suppmat);
 
-    Position    senspos   (-(sens_thick+supp_thick)/2.+sens_thick/2.,0,0);
-    Position    supppos   (-(sens_thick+supp_thick)/2.+sens_thick+supp_thick/2.,0,0);
+    Position    senspos   (0,0,0);
+    Position    supppos   (0,0,0);
+    // Position    senspos   (-(sens_thick+supp_thick)/2.+sens_thick/2.,0,0);
+    // Position    supppos   (-(sens_thick+supp_thick)/2.+sens_thick+supp_thick/2.,0,0);
       
     sensvol.setVisAttributes(lcdd.visAttributes(x_layer.visStr()));
     sensvol.setSensitiveDetector(sens);
@@ -61,7 +71,7 @@ static Ref_t create_element(LCDD& lcdd, const xml_h& e, SensitiveDetector& sens)
     VXDData::Layer layer;
     // Add ladder layer
     layer.NLadders     = nLadders ;
-    layer.internalPhi0 = M_PI/2. - dphi;   // phi0 given wrt x-axis  - internalPhi0 is wrt yaxis , negative orientation
+    layer.internalPhi0 = phi0 ; //M_PI/2. - dphi;   // phi0 given wrt x-axis  - internalPhi0 is wrt yaxis , negative orientation
     layer.Distance     = 0.0; // What is this really (?) : ladderDistance;
     layer.Offset       = offset;              // offset given in direction of positive rotation (increasing phi)
     layer.Thickness    = supp_thick;
@@ -72,8 +82,8 @@ static Ref_t create_element(LCDD& lcdd, const xml_h& e, SensitiveDetector& sens)
 
     // Now add sensitive ladder layer
     layer.NLadders     = nLadders ;
-    layer.internalPhi0 = M_PI/2. - dphi;  // phi0 given wrt x-axis  - internalPhi0 is wrt yaxis , negative orientation
-    layer.Distance     = 0.0; // What is this really (?) : sensitiveDistance ;
+    layer.internalPhi0 = phi0 ;  //M_PI/2. - dphi;  // phi0 given wrt x-axis  - internalPhi0 is wrt yaxis , negative orientation
+    layer.Distance     = 0.0;    // What is this really (?) : sensitiveDistance ;
     layer.Offset       = offset;  // offset given in direction of positive rotation (increasing phi)
     layer.Thickness    = sens_thick;
     layer.Length       = sensbox.x();
@@ -81,13 +91,31 @@ static Ref_t create_element(LCDD& lcdd, const xml_h& e, SensitiveDetector& sens)
     layer.RadLength    = sensmat->GetMaterial()->GetRadLen();
     vxd_data->_sVec.push_back(layer);
 
+
     for(int j=0; j<nLadders; ++j) {
-      double dj = double(j);
+
+      double dj = double(j);      
+      double phi = phi0 + dj*dphi  ;
+
       string laddername = layername + _toString(j,"_ladder%d");
-      Position pos(radius*cos(j*dphi) - offset*sin(dj*dphi),
-		   radius*sin(j*dphi) - offset*cos(dj*dphi),0.);
-      mother.placeVolume(laddervol,pos,Rotation(0,0,dj*dphi));
+
+      // Position pos(radius*cos(dj*dphi) - offset*sin(dj*dphi),
+      // 		   radius*sin(dj*dphi) - offset*cos(dj*dphi),0.);
+
+      double lthick = sens_thick + supp_thick ;
+      
+      Position pos( (radius + lthick/2.)*cos(phi)  - offset * sin( phi ) ,
+       		    (radius + lthick/2.)*sin(phi)  + offset * cos( phi ) ,
+       		    0. ) ;
+
+      Rotation rot( 0, 0 , phi ) ;
+      // Rotation rot(0,0,0) ;
+      // rot.rotateZ(  phi ) ;
+      
+      //      if( dj < 3 ) 
+      mother.placeVolume(laddervol,pos, rot   );
     }
+    
     vxd.setVisAttributes(lcdd, x_det.visStr(),laddervol);
   }
   return vxd;
