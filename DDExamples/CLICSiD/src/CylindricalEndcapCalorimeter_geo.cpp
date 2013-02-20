739a37b87b76ec8691c7a2d1bc3b60ee4b305f22
Markus Frank <markus.frank@cern.ch>
2013-02-20 16:30:36 +0000

    Fix bug spotted by Andre Sailer

--- DDExamples/CLICSiD/src/CylindricalEndcapCalorimeter_geo.cpp
+++ DDExamples/CLICSiD/src/CylindricalEndcapCalorimeter_geo.cpp
@@ -20,7 +20,7 @@ static Ref_t create_detector(LCDD& lcdd, const xml_h& e, SensitiveDetector& sens
   string     det_name  = x_det.nameStr();
   Tube       envelope;
   Volume     envelopeVol(det_name,envelope,air);
-  bool       reflect   = dim.reflect();
+  bool       reflect   = x_det.reflect();
   double     zmin      = dim.inner_z();
   double     rmin      = dim.inner_r();
   double     rmax      = dim.outer_r();
