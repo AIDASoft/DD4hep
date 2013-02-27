f035694866a146536bb030fccda1d51f21cd87ce
Markus Frank <markus.frank@cern.ch>
2013-02-27 12:13:40 +0000

    Example build file.

--- doc/build.sh
+++ doc/build.sh
@@ -4,13 +4,13 @@ GCC_VSN=$2;
 export LCG_external_area=/afs/cern.ch/sw/lcg/external;
 if test -z "$GCC_VSN";
     then
-    GCC_VSN=46;
+    GCC_VSN=gcc46;
     echo "Using default gcc version:${GCC_VSN}";
     fi;
 if test -z "$1";
     then
     echo "You must supply the architecture as first argument:";
-    echo "$0 [os-version:slc5,slc6]  [gcc-version:43,46]";
+    echo "$0 [os-version:slc5,slc6]  [gcc-version:gcc43,gcc46]";
 else  
     svn co https://svnsrv.desy.de/basic/aidasoft/DD4hep/trunk DD4hep
     mkdir ${ARCH_OS};
@@ -24,11 +24,11 @@ else
 	then
 	. ${LCG_external_area}/gcc/4.3/${ARCH}/setup.sh /afs/cern.ch/sw/lcg/external;
 	fi;
-    export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.04/${ARCH}-gcc${GCC_VSN}-dbg/root;
+    export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.04/${ARCH}-${GCC_VSN}-dbg/root;
     ls -laF ${ROOTSYS}/bin
 
 
-    export PATH=${LCG_external_area}/CMake/2.8.6/${ARCH}-gcc${GCC_VSN}-opt/bin:${ROOTSYS}/bin:$PATH;
+    export PATH=${LCG_external_area}/CMake/2.8.6/${ARCH}-${GCC_VSN}-opt/bin:${ROOTSYS}/bin:$PATH;
     cmake -DCMAKE_BUILD_TYPE=Debug -DDD4HEP_WITH_GEANT4=OFF -DDD4HEP_USE_PYROOT=OFF ../DD4hep;
     . thisdd4hep.sh;
     make -j 5;
