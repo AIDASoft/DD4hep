b178c8fa441ee7ddfa2395e5414c76a2cf363e14
Frank Gaede <frank.gaede@desy.de>
2013-05-14 10:55:49 +0000

    - add example libraries and rootmap files to install target

--- cmake/DD4hep.cmake
+++ cmake/DD4hep.cmake
@@ -13,6 +13,11 @@ function(dd4hep_generate_rootmap library)
 		             ${ROOT_genmap_CMD} -i ${libname} -o ${rootmapfile}
                      DEPENDS ${library})
   add_custom_target(${library}Rootmap ALL DEPENDS ${rootmapfile})
+
+  install(FILES ${LIBRARY_OUTPUT_PATH}/${rootmapfile}
+    DESTINATION lib
+    )
+
   # Notify the project level target
   #gaudi_merge_files_append(Rootmap ${library}Rootmap ${CMAKE_CURRENT_BINARY_DIR}/${library}.rootmap)
 endfunction()
@@ -25,7 +30,11 @@ endfunction()
 #---------------------------------------------------------------------------------------------------
 function(dd4hep_install_library library)
   set(installfile ${library}.install)
-  set(libname ${CMAKE_SHARED_MODULE_PREFIX}${library}${CMAKE_SHARED_MODULE_SUFFIX})
+
+  set(rootmapfile ${CMAKE_SHARED_MODULE_PREFIX}${library}.rootmap)
+
+   set(libname ${CMAKE_SHARED_MODULE_PREFIX}${library}${CMAKE_SHARED_MODULE_SUFFIX})
+
   add_custom_command(OUTPUT ${rootmapfile}
                      COMMAND echo
 		             ${library} ${LIBRARY_OUTPUT_DIR}
