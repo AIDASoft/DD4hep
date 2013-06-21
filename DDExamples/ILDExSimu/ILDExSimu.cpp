1c463e86e354441ed3f685a35b8191c460ee5df6
Frank Gaede <frank.gaede@desy.de>
2013-06-21 15:43:23 +0000

    - added the option to read a gdml file into ILDExSimu:    ./bin/ILDExSimu  -g ./ILD_toy.gdml    - use as cross check with gdml files created by      geoConverter    - remove volid tags beforehand:      cat ILD_toy.gdml | grep -i physvolid > ILD_toy_fixed.gdml

--- DDExamples/ILDExSimu/ILDExSimu.cpp
+++ DDExamples/ILDExSimu/ILDExSimu.cpp
@@ -26,6 +26,8 @@
 #include "ILDExSteppingVerbose.h"
 
 #include "DDG4/Geant4DetectorConstruction.h"
+#include "DDG4/Geant4GDMLDetector.h"
+
 #include "DD4hep/LCDD.h"
 
 // -- lcio --
@@ -59,18 +61,45 @@ int main(int argc,char** argv)
     std::cout << " --- Usage: \n " 
 	      << "  [1] ./bin/ILDExSimu  file:../DDExamples/ILDExDet/compact/ILDEx.xml file:../DDExamples/ILDExDet/compact/geant4.xml run1_g4.mac \n"
 	      << "  [2] ./bin/ILDExSimu  -i file:../DDExamples/ILDExDet/compact/ILDEx.xml file:../DDExamples/ILDExDet/compact/geant4.xml \n"
-	      << "  [1]: batch mode - [2]: interactive " << std::endl ;
+	      << "  [3] ./bin/ILDExSimu  -g ./ILD_toy.gdml \n" 
+	      << "  [1]: batch mode - [2]: interactive [3]: interactive reading a gdml file " << std::endl ;
     exit( 0 ) ;
   }
 
-  bool isBatchMode = ( std::string( argv[1] ) != "-i" ) ;
+
+
+//**************************************************************
+
+  bool isReadGDML  = ( std::string( argv[1] ) == "-g" ) ;
+
+  bool isBatchMode = ( std::string( argv[1] ) != "-i" && !isReadGDML  ) ;
+
   int argStart = ( isBatchMode ?       1 : 2 ) ; 
   int argEnd   = ( isBatchMode ?  argc-1 : argc ) ; 
 
-  for(int i=argStart; i < argEnd ; ++i ) {
+  if( ! isReadGDML ) {
+    
+    for(int i=argStart; i < argEnd ; ++i ) {
+      
+      // We need to construct the geometry at this level already
+      lcdd.fromCompact(argv[i]);
+    }
+  }
 
-    // We need to construct the geometry at this level already
-    lcdd.fromCompact(argv[i]);
+  //**************************************************************
+
+  // Get the detector constructed
+  //
+  //  G4VUserDetectorConstruction* detector  = ( isReadGDML ?    new Geant4GDMLeDetector( argv[2] )   :   new Geant4DetectorConstruction(lcdd)  ) ;
+  G4VUserDetectorConstruction* detector  =  0 ;
+  if( isReadGDML )  {
+    
+    detector  = new Geant4GDMLDetector( argv[2] ) ;
+
+  } else {
+    
+    detector  =  new Geant4DetectorConstruction( lcdd )   ;
+
   }
 
   // User Verbose output class
@@ -81,34 +110,36 @@ int main(int argc,char** argv)
   //
   G4RunManager * runManager = new G4RunManager;
   
-  // Get the detector constructed
-  //
-  Geant4DetectorConstruction* detector = new Geant4DetectorConstruction(lcdd);
+  
   runManager->SetUserInitialization(detector);
   
   //
   G4VUserPhysicsList* physics = new QGSP_BERT ; //new ILDExPhysicsList;
   runManager->SetUserInitialization(physics);
   
-  // Set user action classes
-  //
-  G4VUserPrimaryGeneratorAction* gen_action = 
-  new ILDExPrimaryGeneratorAction(lcdd);
-  runManager->SetUserAction(gen_action);
 
-  //---
-  ILDExRunAction* run_action = new ILDExRunAction;  
-  run_action->runData.lcioWriter = lcWrt ;
-  run_action->runData.detectorName = lcdd.header().name() ;
-  runManager->SetUserAction(run_action);
-
-  //
-  ILDExEventAction* event_action = new ILDExEventAction(run_action);
-  runManager->SetUserAction(event_action);
-  //
-  G4UserSteppingAction* stepping_action =
-  new ILDExSteppingAction(event_action);
-  runManager->SetUserAction(stepping_action);
+  if( ! isReadGDML ) {
+    // Set user action classes
+    //
+    G4VUserPrimaryGeneratorAction* gen_action = 
+      new ILDExPrimaryGeneratorAction(lcdd);
+    runManager->SetUserAction(gen_action);
+    
+    //---
+    ILDExRunAction* run_action = new ILDExRunAction;  
+    run_action->runData.lcioWriter = lcWrt ;
+    run_action->runData.detectorName = lcdd.header().name() ;
+    runManager->SetUserAction(run_action);
+    
+    //
+    ILDExEventAction* event_action = new ILDExEventAction(run_action);
+    runManager->SetUserAction(event_action);
+    //
+    G4UserSteppingAction* stepping_action =
+      new ILDExSteppingAction(event_action);
+    runManager->SetUserAction(stepping_action);
+    
+  }
   
   // Initialize G4 kernel
   //
