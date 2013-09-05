97f93c2e5faa81c50ec274cef706848dc41a60eb
Christoph Rosemann <Christoph.Rosemann@desy.de>
2013-09-05 09:30:26 +0000

    Determine global svn revision and store its value in a cmake variable.

---
+++ cmake/GlobalSVNRevision.cmake
@@ -0,0 +1,11 @@
+EXECUTE_PROCESS( COMMAND "svnversion"
+        OUTPUT_VARIABLE GLOBAL_SVN_REVISION
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/
+        RESULT_VARIABLE _exit_code
+        )
+  IF( NOT _exit_code EQUAL 0 )
+        MESSAGE( STATUS "Couldn't retrieve a version number from SVN to set in documentation.")
+        SET(GLOBAL_SVN_REVISION "NoNumberAvailable")
+    ENDIF()
+
+MESSAGE(STATUS "Found global svn revision to be ${GLOBAL_SVN_REVISION}.")
