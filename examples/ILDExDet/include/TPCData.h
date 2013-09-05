03326f500e65596eabd024545dbdb1dcbd9f7276
Frank Gaede <frank.gaede@desy.de>
2013-09-05 15:18:43 +0000

    - cleaned up version of old example ILDExDet     - is now  build standalone     - needs proper setting of (DY)LD_LIBRARY_PATH

---
+++ examples/ILDExDet/include/TPCData.h
@@ -0,0 +1,27 @@
+// $Id: TPCData.h 513 2013-04-05 14:31:53Z gaede $
+//====================================================================
+//  AIDA Detector description implementation for LCD
+//--------------------------------------------------------------------
+//
+//  Author     : M.Frank
+//
+//====================================================================
+
+#include "DD4hep/Detector.h"
+#include <vector>
+
+namespace DD4hep {
+  
+  
+  struct TPCData : public Geometry::DetElement::Object {
+    typedef Geometry::Ref_t Ref_t;
+    Ref_t outerWall;
+    Ref_t innerWall;
+    Ref_t gasVolume;
+    Ref_t cathode;
+    //positive endplate
+    Ref_t endplate;
+    //negative endplate
+    Ref_t endplate2;
+  };
+}
---
+++ examples/ILDExDet/include/VXDData.h
@@ -0,0 +1,40 @@
+// $Id: VXDData.h 513 2013-04-05 14:31:53Z gaede $
+//====================================================================
+//  AIDA Detector description implementation for LCD
+//--------------------------------------------------------------------
+//
+//  Author     : M.Frank
+//
+//====================================================================
+
+#include "DD4hep/Detector.h"
+#include <vector>
+
+namespace DD4hep {
+
+  /**@class VXDData
+   *
+   *   @author  M.Frank
+   *   @version 1.0
+   */
+  struct VXDData : public Geometry::DetElement::Object {
+    /// Helper class for layer properties
+    struct Layer {
+      double internalPhi0 ;
+      double Distance ;
+      double Offset ;
+      double Thickness ;
+      double Length ;
+      double Width ;
+      double RadLength ;
+      int    NLadders ;
+    };
+    typedef std::vector<Layer> LayerVec ;
+    typedef Geometry::Ref_t Ref_t;
+
+    // Ladder
+    LayerVec _lVec;
+    // Sensitive
+    LayerVec _sVec;
+  };
+}
