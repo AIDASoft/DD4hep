bfd4ebeeb146fce22cfcaf8033869f6b5f246679
Andre Sailer <andre.philippe.sailer@cern.ch>
2013-09-10 16:18:40 +0000

    Add root libs to (DY)LD_LIBRARY_PATH, make cmake find genmap. No longer need to have rootsys/bin in the path.

--- cmake/MakeRootMap.cmake
+++ cmake/MakeRootMap.cmake
@@ -1,15 +1,17 @@
 
 if(APPLE)
   #MESSAGE( STATUS " set DYLD_LIBRARY_PATH to  ./:$ENV{DYLD_LIBRARY_PATH} " )
-  SET ( ENV{DYLD_LIBRARY_PATH} ./:$ENV{DYLD_LIBRARY_PATH} )
+  SET ( ENV{DYLD_LIBRARY_PATH} ./:$ENV{DYLD_LIBRARY_PATH}:$ENV{ROOTSYS}/lib )
 else()
-  SET ( ENV{LD_LIBRARY_PATH} ./:$ENV{LD_LIBRARY_PATH} )
+  SET ( ENV{LD_LIBRARY_PATH} ./:$ENV{LD_LIBRARY_PATH}:$ENV{ROOTSYS}/lib )
 endif()
 
-MESSAGE( STATUS " *** MakeROOTMap.cmake run command : genmap -debug -i ${libname} -o ${rootmapfile}
+FIND_PROGRAM( ROOT_genmap_CMD genmap PATHS $ENV{ROOTSYS}/bin $ENV{PATH} )
+
+MESSAGE( STATUS " *** MakeROOTMap.cmake run command : ${ROOT_genmap_CMD} -debug -i ${libname} -o ${rootmapfile}
                  WORKING_DIRECTORY ${genmap_install_dir} "
 )
 EXECUTE_PROCESS( COMMAND 
-		 genmap -debug -i ${libname} -o ${rootmapfile}
+		 ${ROOT_genmap_CMD} -debug -i ${libname} -o ${rootmapfile}
                  WORKING_DIRECTORY ${genmap_install_dir}
 )
