18fe7efebd5cfeb18709387c27d7025e8f256983
Christian Grefe <Christian.Grefe@cern.ch>
2013-09-16 09:00:40 +0000

    Remove the build files that were not meant to be committed

--- examples/CLICSiD/build/CMakeFiles/CLICSiDRootmap.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/CLICSiDRootmap"
-  "libCLICSiD.rootmap"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/CLICSiDRootmap.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
