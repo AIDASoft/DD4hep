9ca60e5f6de735117eaec302a7ec57ce18b3a01d
Frank Gaede <frank.gaede@desy.de>
2014-01-09 10:14:59 +0000

    - first basic implementation of (unit) testing code    - no real tests so far

---
+++ DDTest/src/example_test.cc
@@ -0,0 +1,55 @@
+#include "DD4hepTest.h"
+#include <exception>
+#include <iostream>
+#include <assert.h>
+#include <cmath>
+
+using namespace std ;
+
+// this should be the first line in your test
+DD4hepTest test = DD4hepTest( "example" ) ; 
+
+//=============================================================================
+
+int main(int argc, char** argv ){
+    
+  try{
+    
+    // ----- write your tests in here -------------------------------------
+
+    test.log( "example test" );
+
+    // ----- example test for testing two expressions for equality:
+
+    test( "Example", "Example", "example test - string comparison " ); // this test will pass
+
+    //test( "Example", "BadExample", "example test - string comparison " ); //  this test will fail
+
+
+
+    double pi_d = M_PI ;
+    float  pi_f = M_PI ;
+
+    // ----- example for test of an expresssion - not using equality
+
+    test(  1.1 * pi_f > pi_d   , " example comparison:  1.1* float(M_PI)  > double( M_PI) " ) ;  
+
+    // ... guess which of these will pass ...
+    // test(  double(pi_f) == pi_d  , " example comparison:  float(M_PI) == double( M_PI) " ) ;  
+    // test(  double(pi_f) <  pi_d  , " example comparison:  float(M_PI)  < double( M_PI) " ) ;  
+    // test(  double(pi_f) >  pi_d  , " example comparison:  float(M_PI)  > double( M_PI) " ) ;  
+
+    // --------------------------------------------------------------------
+
+
+  } catch( exception &e ){
+    //} catch( ... ){
+
+    test.log( e.what() );
+    test.error( "exception occurred" );
+  }
+
+  return 0;
+}
+
+//=============================================================================
