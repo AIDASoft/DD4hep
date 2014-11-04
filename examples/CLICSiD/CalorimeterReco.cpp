f746db9f9df15f2df27a1884504d375c5c3dae92
Christian Grefe <Christian.Grefe@cern.ch>
2014-11-04 17:01:00 +0000

    Updated example to show how to use DDRec interface in PolyhedraBarrelCalorimeter2

--- examples/CLICSiD/CalorimeterReco.cpp
+++ examples/CLICSiD/CalorimeterReco.cpp
@@ -6,32 +6,31 @@
  */
 
 #include "DD4hep/LCDD.h"
-#include "BarrelDetector.h"
-#include "PolyhedralBarrelCalorimeter.h"
-#include "Calorimeter.h"
+#include "DDRec/API/Calorimeter.h"
 
 using namespace std;
 using namespace DD4hep;
+using namespace DDRec;
 using namespace Geometry;
 
 void printCalorimeterInformation(const DetElement& det) {
 	std::string delimiter = "-------------------------------------\n";
 
 	cout << "Detector name: " << det.name() << ", type: " << det.type() << endl;
-	PolyhedralCalorimeter calorimeter(det);
+	Calorimeter calorimeter(det);
 	cout << delimiter << "Layer interface:" << endl;
-	cout << "\tNumber of layers: " << calorimeter.getNumberOfLayers() << endl;
-	cout << "\tTotal thickness: " << calorimeter.getTotalThickness() << " cm" << endl;
-	cout << "\tTotal interaction lengths: " << calorimeter.getTotalInteractionLengths() << " Lambda_int" << endl;
-	cout << "\tTotal radiation lengths: " << calorimeter.getTotalRadiationLengths() << " X_0" << endl;
-	for (int layerIndex = 1; layerIndex <= calorimeter.getNumberOfLayers(); layerIndex++) {
-		cout << "\t\tLayer " << layerIndex << ": d = " << calorimeter.getLayerThickness(layerIndex)
-				<< " cm , Lambda/Lambda_int = " << calorimeter.getInteractionLengths(layerIndex) << ", X/X_0 = "
-				<< calorimeter.getRadiationLengths(layerIndex) << ", Sensors: "
-				<< calorimeter.getNumberOfSensors(layerIndex) << endl;
+	cout << "\tNumber of layers: " << calorimeter.numberOfLayers() << endl;
+	cout << "\tTotal thickness: " << calorimeter.totalThickness() << " cm" << endl;
+	cout << "\tTotal interaction lengths: " << calorimeter.totalInteractionLength() << " Lambda_int" << endl;
+	cout << "\tTotal radiation lengths: " << calorimeter.totalRadiationLength() << " X_0" << endl;
+	for (int layerIndex = 1; layerIndex <= calorimeter.numberOfLayers(); layerIndex++) {
+		cout << "\t\tLayer " << layerIndex << ": d = " << calorimeter.thickness(layerIndex)
+				<< " cm , Lambda/Lambda_int = " << calorimeter.interactionLength(layerIndex) << ", X/X_0 = "
+				<< calorimeter.radiationLength(layerIndex) << ", Sensors: "
+				<< calorimeter.numberOfSensors(layerIndex) << endl;
 	}
 
-	cout << delimiter << "Polyhedra interface:" << endl;
+	cout << delimiter << "Subdetector interface:" << endl;
 	cout << "\tInner radius: " << calorimeter.getRMin() << " cm" << endl;
 	cout << "\tOuter radius: " << calorimeter.getRMax() << " cm" << endl;
 	cout << "\tMinimum z: " << calorimeter.getZMin() << " cm" << endl;
