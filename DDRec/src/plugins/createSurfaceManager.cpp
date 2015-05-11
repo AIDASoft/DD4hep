53e71520338b1355b95cb7ca9e43b301f68146a5
Frank Gaede <frank.gaede@desy.de>
2015-05-11 14:12:54 +0000

    implemented createSurfaceManager plugin (InstallSurfaceManager)

--- DDRec/src/plugins/createSurfaceManager.cpp
+++ DDRec/src/plugins/createSurfaceManager.cpp
@@ -1,6 +1,8 @@
 #include "DD4hep/LCDD.h"
 #include "DD4hep/Factories.h"
-#include "DD4hep/DD4hepUnits.h" 
+#include "DD4hep/Printout.h"
+
+#include "DDRec/SurfaceManager.h"
 
 namespace DD4hep{
   namespace DDRec{
@@ -8,29 +10,25 @@ namespace DD4hep{
     using namespace Geometry ;
 
     /** Plugin that creates a SurfaceManager object and attaches
-     *  to lcdd as user extension.
+     *  it to lcdd as a user extension object.
      * 
      *  @author  F.Gaede, CERN/DESY
      *  @date May, 11 2015
      *  @version $Id: $
      */
-    
     static long createSurfaceManager(LCDD& lcdd, int /*argc*/, char** /*argv*/) {
-      
 
-      std::cout << " ************* " <<  std::endl
-		<< " **** running plugin SurfaceManagerPlugin ! " <<  std::endl
-		<< " ************* " <<  std::endl
-		<<  std::endl ;
-      
+      printout(INFO,"InstallSurfaceManager","**** running plugin InstallSurfaceManager ! " );
+
+      lcdd.addExtension<SurfaceManager>(  new SurfaceManager() ) ;
+
+      printout(INFO,"InstallSurfaceManager","%s" , lcdd.extension<SurfaceManager>()->toString().c_str() );
 
       return 1;
     }
-
-
   }
 }
 
-DECLARE_APPLY( SurfaceManagerPlugin, DD4hep::DDRec::createSurfaceManager )
+DECLARE_APPLY( InstallSurfaceManager, DD4hep::DDRec::createSurfaceManager )
 
 
