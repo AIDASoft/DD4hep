32605af467713c2f54ced116079ecaa0778feb4e
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-06-18 17:19:38 +0000

    DDG4: Fix output when getting exception in sensitive detector

--- DDG4/plugins/Geant4SDActions.cpp
+++ DDG4/plugins/Geant4SDActions.cpp
@@ -97,7 +97,7 @@ namespace DD4hep {
 	    << " Post (" <<std::setw(24) << step->GetPostStepPoint()->GetMomentum() << ") "
 	    << std::endl;
 
-	std::cout << out;
+	std::cout << out.str();
 
 	return true;
       }
