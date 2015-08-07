51820e2e5e82f3758504d630769cb8ac0e459969
Markus Frank <markus.frank@cern.ch>
2015-08-07 01:56:32 +0000

    Fix examples in nightlies

--- cmake/thisdd4hep.sh
+++ cmake/thisdd4hep.sh
@@ -1,77 +1,98 @@
-
+#!/bin/bash
 # Source this script to set up the DD4Hep installation that this script is part of.
 #
 # This script if for the csh like shells, see thisdd4hep.csh for csh like shells.
 #
-# Author: Pere Mato. F. Gaede
-
-
+# Author: Pere Mato. F. Gaede, M.Frank
+#-------------------------------------------------------------------------------
+#
 #echo " ### thisdd4hep.sh:   initialize the environment for DD4hep ! " 
-
-if [ "x${BASH_ARGV[0]}" = "x" ]; then
-    if [ ! -f bin/thisdd4hep.sh ]; then
-        echo ERROR: must "cd where/dd4hep/is" before calling ". bin/thisdd4hep.sh" for this version of bash!
-        return 1
-    fi
-else
-    # get param to "."
-    THIS=$(dirname ${BASH_ARGV[0]})
-fi
-
-
-#----------- source the ROOT environment first
-source @ROOT_ROOT@/bin/thisroot.sh
-
-if [ @DD4HEP_USE_GEANT4@ ]
-then
-    export G4INSTALL=@Geant4_ROOT@
-    export G4ENV_INIT=@Geant4_ROOT@/bin/geant4.sh
-
-# ---------- initialze geant4 environment
-    test -r ${G4ENV_INIT} && { cd $(dirname ${G4ENV_INIT}) ; . ./$(basename ${G4ENV_INIT}) ; cd $OLDPWD ; }
-
-
-#---- if geant4 was built with external CLHEP we have to extend the dynamic search path
-    if [ @GEANT4_USE_CLHEP@ ] ; then
-	if [ @USE_DYLD@ ]
-	then
-	    export DYLD_LIBRARY_PATH=@CLHEP_LIBRARY_PATH@:$DYLD_LIBRARY_PATH
-	else
-	    export LD_LIBRARY_PATH=@CLHEP_LIBRARY_PATH@:$LD_LIBRARY_PATH
+#
+dd4hep_parse_this()   {
+    package=${2};
+    if [ "x${1}" = "x" ]; then
+	if [ ! -f bin/this${package}.sh ]; then
+            echo ERROR: must "cd where/${package}/is" before calling ". bin/this${package}.sh" for this version of bash!;
+            return 1;
 	fi
-    fi
-fi
-
-#----PATH-----------------------------------------------------------------
-if [ -z "${PATH}" ]; then
-  PATH=@CMAKE_INSTALL_PREFIX@/bin; export PATH       # Linux, ELF HP-UX
-else
-  PATH=@CMAKE_INSTALL_PREFIX@/bin:$PATH; export PATH
-fi
-
-
-#----(DY)LD_LIBRARY_PATH---------------------------------------------------------------
-if [ @USE_DYLD@ ]
-then
-    if [ -z "${DYLD_LIBRARY_PATH}" ]; then
-	DYLD_LIBRARY_PATH=@CMAKE_INSTALL_PREFIX@/lib:@XERCESC_ROOT_DIR@/lib; export DYLD_LIBRARY_PATH   # Mac OS X
+	THIS="${PWD}";
     else
-	DYLD_LIBRARY_PATH=@CMAKE_INSTALL_PREFIX@/lib:@XERCESC_ROOT_DIR@/lib:$DYLD_LIBRARY_PATH; export DYLD_LIBRARY_PATH
-    fi
-else
-    if [ -z "${LD_LIBRARY_PATH}" ]; then
-	LD_LIBRARY_PATH=@CMAKE_INSTALL_PREFIX@/lib:@XERCESC_ROOT_DIR@/lib:@Geant4_DIR@/..; export LD_LIBRARY_PATH       # Linux, ELF HP-UX
+	# get param to "."
+	THIS=$(dirname $(dirname ${1}));
+	#if [ ! -f ${THIS}/bin/this${package}.sh ]; then
+	#    THIS=$(dirname ${package});
+	#fi;
+    fi;
+    THIS=$(cd ${THIS} > /dev/null; pwd);
+}
+
+dd4hep_add_path()   {
+    path_name=${1};
+    path_prefix=${2};
+    eval path_value=\$$path_name;
+    if test -z "${path_value}"; then
+	path_value=${path_prefix};
     else
-	LD_LIBRARY_PATH=@CMAKE_INSTALL_PREFIX@/lib:@XERCESC_ROOT_DIR@/lib:@Geant4_DIR@/..:$LD_LIBRARY_PATH; export LD_LIBRARY_PATH
-    fi
-fi
+	path_value=${path_prefix}:${path_value};
+    fi; 
+    eval export ${path_name}=${path_value};
+    ## echo "dd4hep_add_path: ${path_name}=${path_value}";
+}
 
-#----PYTHONPATH---------------------------------------------------------------
-if [ -z "${PYTHONPATH}" ]; then
-  PYTHONPATH=@CMAKE_INSTALL_PREFIX@/python:@CMAKE_SOURCE_DIR@/DDCore/python; export PYTHONPATH
-else
-  PYTHONPATH=@CMAKE_INSTALL_PREFIX@/python:@CMAKE_SOURCE_DIR@/DDCore/python:$PYTHONPATH; export PYTHONPATH
-fi
-
+dd4hep_add_library_path()    {
+    path_prefix=${1};
+    if [ @USE_DYLD@ ];
+    then
+	export DYLD_LIBRARY_PATH=${path_prefix}:$DYLD_LIBRARY_PATH;
+    else
+	export LD_LIBRARY_PATH=${path_prefix}:$LD_LIBRARY_PATH;
+    fi;
+}
+#
+dd4hep_parse_this ${BASH_ARGV[0]} DD4hep;
+#
+# These 3 are the main configuration variables: ROOT, Geant4 and XercesC
+# --> LCIO & Co. are handled elsewhere!
+export ROOTSYS=@ROOT_ROOT@;
+export Geant4_DIR=@Geant4_DIR@;
+export XERECESCINSTALL=@XERCESC_ROOT_DIR@;
+#
 #----DD4hep installation directory--------------------------------------------
-DD4hepINSTALL=@CMAKE_INSTALL_PREFIX@; export DD4hepINSTALL;
+export DD4hepINSTALL=${THIS};
+#
+#----------- source the ROOT environment first ------------------------------
+ROOTENV_INIT=${ROOTSYS}/bin/thisroot.sh;
+test -r ${ROOTENV_INIT} && { cd $(dirname ${ROOTENV_INIT}); . ./$(basename ${ROOTENV_INIT}) ; cd $OLDPWD ; }
+#
+#----Geant4 LIBRARY_PATH------------------------------------------------------
+if test -n "${Geant4_DIR}"; then
+    G4LIB_DIR=`dirname ${Geant4_DIR}`;
+    export G4INSTALL=`dirname ${G4LIB_DIR}`;
+    export G4ENV_INIT=${G4INSTALL}/bin/geant4.sh
+    # ---------- initialze geant4 environment
+    test -r ${G4ENV_INIT} && { cd $(dirname ${G4ENV_INIT}) ; . ./$(basename ${G4ENV_INIT}) ; cd $OLDPWD ; }
+    #---- if geant4 was built with external CLHEP we have to extend the dynamic search path
+    if [ @GEANT4_USE_CLHEP@ ] ; then
+	dd4hep_add_library_path @CLHEP_LIBRARY_PATH@;
+    fi
+    dd4hep_add_library_path ${G4LIB_DIR};
+    unset G4ENV_INIT;
+    unset G4LIB_DIR;
+fi
+#
+#----XercesC LIBRARY_PATH-----------------------------------------------------
+if test -n "${XERCESCINSTALL}"; then
+    #dd4hep_add_path    PATH ${XERCESCINSTALL}/bin;
+    dd4hep_add_library_path ${XERCESCINSTALL}/lib;
+fi;
+#
+#----PATH---------------------------------------------------------------------
+dd4hep_add_path PATH       ${THIS}/bin;
+#----LIBRARY_PATH-------------------------------------------------------------
+dd4hep_add_library_path    ${THIS}/lib;
+#----PYTHONPATH---------------------------------------------------------------
+dd4hep_add_path PYTHONPATH ${THIS}/python;
+#
+unset ROOTENV_INIT;
+unset THIS;
+#-----------------------------------------------------------------------------
--- doc/CompileAllOptionPermutations.sh
+++ doc/CompileAllOptionPermutations.sh
@@ -3,9 +3,9 @@ INSTALL_G4=/home/frankm/SW/g4_10.01.p02_dbg/lib/Geant4-10.1.2;
 INSTALL_LCIO=/home/frankm/SW/lcio/v02-04-03;
 INSTALL_XERCESC=/home/frankm/SW/xercesc;
 CHECKOUT=${dir_name}/../../DD4hep.trunk/checkout;
-
 export ROOTSYS=/home/frankm/SW/root_v5.34.25_dbg;
 . ${ROOTSYS}/bin/thisroot.sh;
+#cat ${ROOTSYS}/bin/thisroot.sh;
 #
 LINE="==================================================================================================="
 #
@@ -57,29 +57,37 @@ make_build()
     fi;
 }
 
-for DOGEANT4 in OFF ON; do
-    for DOXERCESC in OFF ON; do
-	for DOGEAR in OFF; do
-	    for DOLCIO in OFF ON; do
-		folder=build_Xer${DOXERCESC}_Geant${DOGEANT4}_Gear${DOGEAR}_Lcio${DOLCIO}
-                WORK_DIR=${dir_name}/${folder};
-		mkdir -p ${WORK_DIR}/EX;
-		cd ${WORK_DIR};
-                make_output;
-                #-DDD4HEP_DEBUG_CMAKE=ON \
-		unset DD4hep_DIR;
-                OPTS="`make_opt ${DOGEANT4} -DDD4HEP_USE_GEANT4 -DGeant4_DIR=${INSTALL_G4}`\
+build_all()
+{
+    for DOGEANT4 in OFF ON; do
+	for DOXERCESC in OFF ON; do
+	    for DOGEAR in OFF; do
+		for DOLCIO in OFF ON; do
+		    folder=build_Xer${DOXERCESC}_Geant${DOGEANT4}_Gear${DOGEAR}_Lcio${DOLCIO}
+                    WORK_DIR=${dir_name}/${folder};
+		    mkdir -p ${WORK_DIR}/EX;
+		    cd ${WORK_DIR};
+                    make_output;
+                    #-DDD4HEP_DEBUG_CMAKE=ON \
+		    unset DD4hep_DIR;
+                    OPTS="`make_opt ${DOGEANT4} -DDD4HEP_USE_GEANT4 -DGeant4_DIR=${INSTALL_G4}`\
 		    `make_opt ${DOLCIO}     -DDD4HEP_USE_LCIO -DLCIO_DIR=${INSTALL_LCIO}` \
 		    `make_opt ${DOXERCESC}  -DDD4HEP_USE_XERCESC -DXERCESC_ROOT_DIR=${INSTALL_XERCESC}` \
+                    -DROOTSYS=${ROOTSYS} \
                     -DCMAKE_INSTALL_PREFIX=${WORK_DIR}/DD4hep";
-		CMD="cd ${dir_name}/$folder ; cmake ${OPTS} ${CHECKOUT};";
-                make_build;
-                DD4hep_DIR=${WORK_DIR}/DD4hep;
-   		CMD="cd ${WORK_DIR}/EX; cmake ${OPTS} -DDD4hep_DIR=${DD4hep_DIR} ${CHECKOUT}/examples;";
-                make_build;
-                #
-		cd ../..;
-	    done
-	done
-    done
-done
+		    CMD="cd ${dir_name}/$folder ; cmake ${OPTS} ${CHECKOUT};";
+                    make_build;
+
+		    unset ROOTSYS;
+                    DD4hep_DIR=${WORK_DIR}/DD4hep;
+   		    CMD="cd ${WORK_DIR}/EX; cmake ${OPTS} -DDD4hep_DIR=${DD4hep_DIR} ${CHECKOUT}/examples;";
+                    make_build;
+                    #
+		    cd ../..;
+		done;
+	    done;
+	done;
+    done;
+}
+
+build_all;
