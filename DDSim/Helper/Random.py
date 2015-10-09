4fc73eaf493f1511ef27a03eccd65425f55fe26a
Andre Sailer <andre.philippe.sailer@cern.ch>
2015-10-09 17:07:06 +0000

    DDSim: add configuration for random generator

---
+++ DDSim/Helper/Random.py
@@ -0,0 +1,13 @@
+"""Helper object for random number generator objects"""
+
+from DDSim.Helper.ConfigHelper import ConfigHelper
+
+class Random (ConfigHelper):
+  """Properties for random number generator"""
+  def __init__ (self):
+    super(Random, self).__init__()
+    self.seed = None
+    self.type = None
+    self.luxury = 1
+    self.replace_gRandom = True
+    self.file = None
