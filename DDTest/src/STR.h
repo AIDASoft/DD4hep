269df28ca03828210ed83cf2f7c453e6478123cb
Markus Frank <markus.frank@cern.ch>
2015-12-11 09:03:35 +0000

    Missing commit....

---
+++ DDTest/src/STR.h
@@ -0,0 +1,54 @@
+//  AIDA Detector description implementation for LCD
+//--------------------------------------------------------------------------
+// Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
+// All rights reserved.
+//
+// For the licensing terms see $DD4hepINSTALL/LICENSE.
+// For the list of contributors see $DD4hepINSTALL/doc/CREDITS.
+//
+//  \author   F.Gaede
+//  \date     2015-12-11
+//  \version  1.0
+//
+//==========================================================================
+// $Id$
+#ifndef STR_H 
+#define STR_H 1
+
+#include <sstream>
+#include <string>
+
+namespace  {
+
+  template <typename T> std::string _to_string(const T& _val)  {
+    std::stringstream res; 
+    res << _val ;
+    return res.str();
+  }
+
+  /// Helper class for floating point comparisons using string representations
+  /**
+   *  \author  F.Gaede
+   *  \date    2015-12-11
+   *  \version 1.0
+   *
+   */
+  class STR {
+    STR() {} 
+    float _val ;
+    std::string _str ;
+  public:
+    STR ( float val ) : _val(val), _str(_to_string(val))  {   }
+    std::string str() const { return _str ; }
+    bool operator==( const STR& s2) const {
+      return this->str() == s2.str() ;
+    }
+  };
+}
+
+inline std::ostream& operator<<(std::ostream& os , const STR& s) {
+  os << s.str() ;
+  return os ;
+} 
+
+#endif // STR_H
