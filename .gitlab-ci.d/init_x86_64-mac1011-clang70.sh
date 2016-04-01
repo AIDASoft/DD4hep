50c038ba3b876eb8876e1bcbefd069a7fc0ff47c
Marko Petric <marko.petric@cern.ch>
2016-04-01 10:36:09 +0000

    The link from sft to geant4 cvmfs is in place, let's test if it works

--- .gitlab-ci.d/init_x86_64-mac1011-clang70.sh
+++ .gitlab-ci.d/init_x86_64-mac1011-clang70.sh
@@ -1,58 +1,69 @@
-#!/bin/bash
- 
-# General variables
-export MACRELEASES=/Users/gitlab-runner/CI
+# -------------------------------------------------------------------- ---
 
-#--------------------------------------------------------------------------------
-#     CLHEP
-#--------------------------------------------------------------------------------
+# ---  Use the same compiler and python as used for the installation   ---
 
-export CLHEP=$MACRELEASES/clhep-2.3.1.0
-export CLHEP_BASE_DIR="$CLHEP"
-export CLHEP_INCLUDE_DIR="$CLHEP/include"
-export PATH="$CLHEP_BASE_DIR/bin:$PATH"
-export LD_LIBRARY_PATH="$CLHEP_BASE_DIR/lib:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$CLHEP_BASE_DIR/lib:$DYLD_LIBRARY_PATH"
+# -------------------------------------------------------------------- ---
+export PATH=/usr/bin:/usr/bin:${PATH}
+export LD_LIBRARY_PATH=/usr/lib:/usr/lib64:/usr/lib:${LD_LIBRARY_PATH}
 
-#--------------------------------------------------------------------------------
-#     Geant4    
-#--------------------------------------------------------------------------------
-
-export G4INSTALL=$MACRELEASES/geant4.10.02
-export G4ENV_INIT="$G4INSTALL/bin/geant4.sh"
-export G4SYSTEM="Linux-g++"
 
 #--------------------------------------------------------------------------------
 #     ROOT
 #--------------------------------------------------------------------------------
-
-export ROOTSYS=$MACRELEASES/root-6.06.02
+export ROOTSYS="/Users/gitlab-runner/CI/root_v6.06.02"
 export PYTHONPATH="$ROOTSYS/lib:$PYTHONPATH"
 export PATH="$ROOTSYS/bin:$PATH"
 export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$ROOTSYS/lib:$DYLD_LIBRARY_PATH"
+
+
+#--------------------------------------------------------------------------------
+#     CLHEP
+#--------------------------------------------------------------------------------
+export CLHEP="/Users/gitlab-runner/CI/clhep-2.2.0.4"
+export CLHEP_BASE_DIR="$CLHEP"
+export CLHEP_INCLUDE_DIR="$CLHEP/include"
+export PATH="$CLHEP_BASE_DIR/bin:$PATH"
+export LD_LIBRARY_PATH="$CLHEP_BASE_DIR/lib:$LD_LIBRARY_PATH"
+
+#--------------------------------------------------------------------------------
+#     DD4hep
+#--------------------------------------------------------------------------------
+export DD4hepINSTALL=`pwd`
+export DD4HEP=`pwd`
+export PYTHONPATH="$DD4HEP/python:$DD4HEP/DDCore/python:$PYTHONPATH"
+export PATH="$DD4HEP/bin:$PATH"
+export LD_LIBRARY_PATH="$DD4HEP/lib:$LD_LIBRARY_PATH"
 
 #--------------------------------------------------------------------------------
 #     LCIO
 #--------------------------------------------------------------------------------
-
-export LCIO=$MACRELEASES/lcio
+export LCIO="/Users/gitlab-runner/CI/lcio/HEAD"
 export PYTHONPATH="$LCIO/src/python:$LCIO/examples/python:$PYTHONPATH"
 export PATH="$LCIO/tools:$LCIO/bin:$PATH"
 export LD_LIBRARY_PATH="$LCIO/lib:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$LCIO/lib:$DYLD_LIBRARY_PATH"
+
+
+#--------------------------------------------------------------------------------
+#     Geant4
+#--------------------------------------------------------------------------------
+export G4INSTALL="/Users/gitlab-runner/CI/geant4.10.01.p01"
+export G4ENV_INIT="$G4INSTALL/bin/geant4.sh"
+export G4SYSTEM="Darwin-g++"
+
 
 #--------------------------------------------------------------------------------
 #     Boost
 #--------------------------------------------------------------------------------
-
-export BOOST_ROOT=$MACRELEASES/boost_1.60
+export BOOST_ROOT="/Users/gitlab-runner/CI/boost_1.60"
 
 #--------------------------------------------------------------------------------
-#     XercesC
+#     CMake
 #--------------------------------------------------------------------------------
 
-export XercesC_HOME=$MACRELEASES/xerces-c-3.1.3
-export PATH="$XercesC_HOME/bin:$PATH"
-export LD_LIBRARY_PATH="$XercesC_HOME/lib:$LD_LIBRARY_PATH"
-export DYLD_LIBRARY_PATH="$XercesC_HOME/lib:$DYLD_LIBRARY_PATH"
+# --- source GEANT4 INIT script ---
+test -r ${G4ENV_INIT} && { cd $(dirname ${G4ENV_INIT}) ; . ./$(basename ${G4ENV_INIT}) ; cd $OLDPWD ; }
+
+# --- set DYLD_LIBRARY_PATH to LD_LIBRARY_PATH for MAC compatibility ---
+export DYLD_LIBRARY_PATH=$LD_LIBRARY_PATH:$DYLD_LIBRARY_PATH
+
+
