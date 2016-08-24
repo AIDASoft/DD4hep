6b25cfa96998f24915c62e51a05743b59d07be4d
Marko Petric <marko.petric@cern.ch>
2016-08-24 13:40:48 +0000

    Update CI setup

--- .gitlab-ci.d/init_x86_64.sh
+++ .gitlab-ci.d/init_x86_64.sh
@@ -41,7 +41,7 @@ export PATH=${CMAKE_HOME}/bin:$PATH
 #     Python
 #--------------------------------------------------------------------------------
 
-export PYTHONDIR=${CLICREPO}/software/Python/2.7.11/${BUILD_FLAVOUR}
+export PYTHONDIR=${CLICREPO}/software/Python/2.7.12/${BUILD_FLAVOUR}
 export PATH=$PYTHONDIR/bin:$PATH
 export LD_LIBRARY_PATH=$PYTHONDIR/lib:$LD_LIBRARY_PATH
 
@@ -75,7 +75,7 @@ export G4SYSTEM="Linux-g++"
 #--------------------------------------------------------------------------------
 #     LCIO
 #--------------------------------------------------------------------------------
-export LCIO=${CLICREPO}/software/LCIO/2.7.1/${BUILD_FLAVOUR}
+export LCIO=${CLICREPO}/software/LCIO/2.7.2/${BUILD_FLAVOUR}
 # export PYTHONPATH="$LCIO/src/python:$LCIO/examples/python:$PYTHONPATH"
 export PATH="$LCIO/bin:$PATH"
 export LD_LIBRARY_PATH="$LCIO/lib:$LD_LIBRARY_PATH"
