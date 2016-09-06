2c3da1df5b2fc39010ab8a09895fd70f0565ec5d
Markus Frank <markus.frank@cern.ch>
2016-09-06 15:34:25 +0000

    Screwed up test scripts. Reverted back.

--- cmake/run_test_package.sh
+++ cmake/run_test_package.sh
@@ -5,16 +5,10 @@
 #   calls the command (given as first argument)
 #   with all following arguments
 #
-if [ ! ${DD4hep_DIR} ]; then
-    export DD4hep_DIR=@DD4hep_DIR@;
-fi;
-#
-source ${DD4hep_DIR}/bin/thisdd4hep.sh;
-dd4hep_parse_this ${BASH_ARGV[0]} @PackageName@;
-#
+
 #----- initialize environment for this package - including DD4hep 
-source ${THIS}/bin/this@PackageName@.sh
-#
+source @CMAKE_INSTALL_PREFIX@/bin/this@PackageName@.sh
+
 #----- parse command line - first argument is the 
 #      test to run
 command=$1
