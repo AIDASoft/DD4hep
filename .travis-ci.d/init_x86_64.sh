5f653131bc6589218ad40bc7d90a911c74ddc8ac
Marko Petric <marko.petric@cern.ch>
2016-09-14 11:22:37 +0200

    Refactor testing enviroment

--- .travis-ci.d/init_x86_64.sh
+++ .travis-ci.d/init_x86_64.sh
@@ -12,7 +12,7 @@ fi
 
 
 if [ -z ${GCC_VERSION} ]; then
-    GCC_VERSION=4.8.5
+    GCC_VERSION=4.9.3
 fi
   
 if [ -z ${BUILD_TYPE} ]; then
@@ -94,26 +94,3 @@ export LD_LIBRARY_PATH="${BOOST_ROOT}/lib:$LD_LIBRARY_PATH"
 
 export Ninja_HOME=${CLICREPO}/software/Ninja/1.7.1/${BUILD_FLAVOUR}
 export PATH="$Ninja_HOME:$PATH"
-
-
-#--------------------------------------------------------------------------------
-#     BUILD
-#--------------------------------------------------------------------------------
-
-export GCC_VERSION="4.8.5"
-export BUILD_TYPE="opt"
-cd /DD4hep
-mkdir build
-cd build
-cmake -GNinja -D DD4HEP_USE_GEANT4=ON -D DD4HEP_USE_BOOST=ON -DBoost_NO_BOOST_CMAKE=ON -D DD4HEP_USE_LCIO=ON -D BUILD_TESTING=ON  -D Geant4_DIR=$G4INSTALL/lib64/Geant4-10.2.2 -D DD4HEP_USE_CXX11=ON -DCMAKE_BUILD_TYPE=Release ..
-ninja
-ninja install
-. ../bin/thisdd4hep.sh
-ctest --output-on-failure -j4
-cd ../examples/
-mkdir build
-cd build
-cmake -GNinja -DBoost_NO_BOOST_CMAKE=ON ..
-ninja
-ninja install
-ctest --output-on-failure
