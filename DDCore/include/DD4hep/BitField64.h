831081585079224a4b80c1520505d01c3e80289e
Markus Frank <Markus.Frank@cern.ch>
2016-10-20 13:20:06 +0200

    Add wrapper to DDSegmentation/BitField64.h.

---
+++ DDCore/include/DD4hep/BitField64.h
@@ -0,0 +1,30 @@
+//==========================================================================
+//  AIDA Detector description implementation for LCD
+//--------------------------------------------------------------------------
+// Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
+// All rights reserved.
+//
+// For the licensing terms see $DD4hepINSTALL/LICENSE.
+// For the list of contributors see $DD4hepINSTALL/doc/CREDITS.
+//
+// Author     : M.Frank
+//
+//==========================================================================
+#ifndef DD4HEP_GEOMETRY_BITFIELD64_H
+#define DD4HEP_GEOMETRY_BITFIELD64_H
+
+// Framework include files
+#include "DDSegmentation/BitField64.h"
+
+/// Namespace for the AIDA detector description toolkit
+namespace DD4hep {
+
+  /// Namespace for the geometry part of the AIDA detector description toolkit
+  namespace Geometry {
+
+    using DDSegmentation::BitField64;
+    using DDSegmentation::BitFieldValue;
+
+  }       /* End namespace Geometry           */
+}         /* End namespace DD4hep             */
+#endif    /* DD4HEP_GEOMETRY_BITFIELD64_H     */
