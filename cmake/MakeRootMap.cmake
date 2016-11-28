44940abdca8328b253b5fc7e49b53c8cda91cea2
Marko Petric <marko.petric@cern.ch>
2016-11-28 18:12:54 +0100

    Remove ROOT5 things and make approprite fixed to the usage of ROOTConfig.cmake

--- cmake/MakeRootMap.cmake
+++ cmake/MakeRootMap.cmake
@@ -1,11 +1,10 @@
-message(STATUS " *** ROOT_genmap[Root version: ${ROOT_VERSION_MAJOR}]: Generate map for ${libname} ..." )
-if( ${ROOT_VERSION_MAJOR} GREATER 5 )
+message(STATUS " *** ROOT_genmap[Root version: ${ROOT_VERSION}]: Generate map for ${libname} ..." )
   #
   #  Running listcomponents from Gaudi
   #   
   FIND_PROGRAM( ROOT_genmap_CMD listcomponents PATHS ${genmap_install_dir}/../bin ${DD4hep_DIR}/bin )
   ###MESSAGE( STATUS " *** MakeRootMap ${genmap_install_dir}/../bin $ENV{DD4hep_DIR}/bin ${DD4hep_DIR}" )
-  MESSAGE( STATUS " *** MakeRootMap[${ROOT_VERSION_MAJOR}].cmake run command : ${ROOT_genmap_CMD} -o ${rootmapfile} ${libname} 
+  MESSAGE( STATUS " *** MakeRootMap[${ROOT_VERSION}].cmake run command : ${ROOT_genmap_CMD} -o ${rootmapfile} ${libname}
                  WORKING_DIRECTORY ${genmap_install_dir} "
     ) 
 
@@ -20,24 +19,4 @@ if( ${ROOT_VERSION_MAJOR} GREATER 5 )
     WORKING_DIRECTORY ${genmap_install_dir}
     )
 
-else()
-  #
-  #  Running genmap from ROOT 5
-  #
-  FIND_PROGRAM( ROOT_genmap_CMD genmap PATHS $ENV{ROOTSYS}/bin $ENV{PATH} )
-  MESSAGE( STATUS " *** MakeRootMap[${ROOT_VERSION_MAJOR}].cmake run command : ${ROOT_genmap_CMD} -debug -i ${libname} -o ${rootmapfile}
-                 WORKING_DIRECTORY ${genmap_install_dir} "
-    )
 
-  if(APPLE)
-    SET ( ENV{DYLD_LIBRARY_PATH} ./:$ENV{DYLD_LIBRARY_PATH}:$ENV{DD4HEP_LIBRARY_PATH}:$ENV{ROOTSYS}/lib:${genmap_install_dir} )
-  else()
-    SET ( ENV{LD_LIBRARY_PATH} ./:$ENV{LD_LIBRARY_PATH}:$ENV{ROOTSYS}/lib:${genmap_install_dir} )
-  endif()
-
-  EXECUTE_PROCESS( COMMAND 
-    ${ROOT_genmap_CMD} -debug -i ${libname} -o ${rootmapfile}
-    WORKING_DIRECTORY ${genmap_install_dir}
-    )
-
-endif()
