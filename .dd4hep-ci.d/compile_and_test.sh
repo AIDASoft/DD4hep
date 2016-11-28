f023aa5a95a2dbde3c9c212c5f7569b2ddebe425
Marko Petric <marko.petric@cern.ch>
2016-11-28 19:01:38 +0100

    Fix Travis configuration to use ROOTSYS

--- .dd4hep-ci.d/compile_and_test.sh
+++ .dd4hep-ci.d/compile_and_test.sh
@@ -8,7 +8,7 @@ source /DD4hep/.dd4hep-ci.d/init_x86_64.sh
 cd /DD4hep
 mkdir build
 cd build
-cmake -GNinja -D DD4HEP_USE_GEANT4=ON -D DD4HEP_USE_BOOST=ON -DBoost_NO_BOOST_CMAKE=ON -D DD4HEP_USE_LCIO=ON -D BUILD_TESTING=ON  -D Geant4_DIR=$G4INSTALL/lib64/Geant4-10.2.2 -D DD4HEP_USE_CXX11=ON -DCMAKE_BUILD_TYPE=Release .. && \
+cmake -GNinja -D DD4HEP_USE_GEANT4=ON -D DD4HEP_USE_BOOST=ON -DBoost_NO_BOOST_CMAKE=ON -D DD4HEP_USE_LCIO=ON -D BUILD_TESTING=ON  -D Geant4_DIR=$G4INSTALL/lib64/Geant4-10.2.2 -D DD4HEP_USE_CXX11=ON -DCMAKE_BUILD_TYPE=Release -DROOT_DIR=$ROOTSYS .. && \
 ninja && \
 ninja install && \
 . ../bin/thisdd4hep.sh && \
