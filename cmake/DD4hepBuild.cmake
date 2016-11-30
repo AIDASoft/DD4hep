8b4ad78c25b98bdbb7734c593682be79f0c58014
Marko Petric <marko.petric@cern.ch>
2016-11-30 09:32:20 +0100

    proper cmake rpath treatment

--- cmake/DD4hepBuild.cmake
+++ cmake/DD4hepBuild.cmake
@@ -58,13 +58,25 @@ macro(dd4hep_set_compiler_flags)
    set(CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS "${CMAKE_SHARED_LIBRARY_CREATE_CXX_FLAGS} -undefined dynamic_lookup")
 #   All this simply does not work!
    if ( APPLE )
-     set(CMAKE_SKIP_BUILD_RPATH            FALSE)
-     set(CMAKE_BUILD_WITH_INSTALL_RPATH    FALSE)
-     set(CMAKE_INSTALL_RPATH               "${CMAKE_BINARY_DIR}/lib")
-     set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
-#     set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-rpath ${CMAKE_BINARY_DIR}/lib")
-#     message( STATUS "Apple settings: CMAKE_INSTALL_RPATH:       ${CMAKE_INSTALL_RPATH}" )
-#     message( STATUS "Apple settings: CMAKE_SHARED_LINKER_FLAGS: ${CMAKE_SHARED_LINKER_FLAGS}" )
+     # use, i.e. don't skip the full RPATH for the build tree
+     SET(CMAKE_SKIP_BUILD_RPATH  FALSE)
+
+     # when building, don't use the install RPATH already
+     # (but later on when installing)
+     SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+
+     SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+
+     # add the automatically determined parts of the RPATH
+     # which point to directories outside the build tree to the install RPATH
+     SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+
+     # the RPATH to be used when installing, but only if it's not a system directory
+     LIST(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES "${CMAKE_INSTALL_PREFIX}/lib" isSystemDir)
+     IF("${isSystemDir}" STREQUAL "-1")
+       SET(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
+     ENDIF("${isSystemDir}" STREQUAL "-1")
    endif()
  endif()
 endmacro(dd4hep_set_compiler_flags)
