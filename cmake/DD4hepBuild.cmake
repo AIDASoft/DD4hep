1cd2642c3adfe7637f3eadd5e135c83eb10d602e
Markus Frank <Markus.Frank@cern.ch>
2016-12-20 14:10:30 +0100

    Remove obsolete build flags

--- cmake/DD4hepBuild.cmake
+++ cmake/DD4hepBuild.cmake
@@ -73,7 +73,7 @@ macro(dd4hep_set_compiler_flags)
     add_definitions(-DDD4HEP_USE_STDCXX=11)
   endif()
 
-  if ( CMAKE_USE_PTHREADS_INIT )
+  if ( THREADS_HAVE_PTHREAD_ARG )
     set ( CMAKE_CXX_FLAGS           "${CMAKE_CXX_FLAGS} -pthread")
     SET ( CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -pthread")
   elif ( CMAKE_THREAD_LIBS_INIT )
