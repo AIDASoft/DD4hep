0705bd66044cc9dc4492b856d47cffe3450f36f5
Whitney Armstrong <warmstrong@anl.gov>
2017-01-31 20:35:53 -0600

    Fixed doxygen so generated source code is indented.
    Having all indentation made browsing the generated doxygen source code
    unbearable.
    
    Also updated the install path for the cmake files so it doesn't pollute
    the install prefix.
    
    	modified:   cmake/DD4hepMacros.cmake
    	modified:   doc/doxygen/DD4hep.css
    	modified:   doc/doxygen/DD4hepFooter.html
    	modified:   doc/doxygen/DD4hepHeader.html

--- cmake/DD4hepMacros.cmake
+++ cmake/DD4hepMacros.cmake
@@ -76,7 +76,7 @@ MACRO( GENERATE_PACKAGE_CONFIGURATION_FILES )
                 CONFIGURE_FILE( "${PROJECT_SOURCE_DIR}/cmake/${arg}.in"
                                 "${PROJECT_BINARY_DIR}/${arg}" @ONLY
                 )
-                INSTALL( FILES "${PROJECT_BINARY_DIR}/${arg}" DESTINATION . )
+                INSTALL( FILES "${PROJECT_BINARY_DIR}/${arg}" DESTINATION cmake )
                 #IF( EXISTS "${_current_dir}/MacroCheckPackageLibs.cmake" )
                 #    INSTALL( FILES "${_current_dir}/MacroCheckPackageLibs.cmake" DESTINATION cmake )
                 #ENDIF()
