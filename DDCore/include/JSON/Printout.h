2ef68d9f16176de310793d2b5c93ef73a4ca84c1
Markus Frank <Markus.Frank@cern.ch>
2017-03-16 13:32:06 +0100

    Add first version of JSON interpreter

---
+++ DDCore/include/JSON/Printout.h
@@ -0,0 +1,24 @@
+//==========================================================================
+//  AIDA Detector description implementation for LCD
+//--------------------------------------------------------------------------
+// Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
+// All rights reserved.
+//
+// For the licensing terms see $DD4hepINSTALL/LICENSE.
+// For the list of contributors see $DD4hepINSTALL/doc/CREDITS.
+//
+// Author     : M.Frank
+//
+//==========================================================================
+#ifndef DD4HEP_DDCORE_JSON_PRINTOUT_H
+#define DD4HEP_DDCORE_JSON_PRINTOUT_H
+
+// Forwarding printout functionality to DD4hep
+/** Note: This is necessary to use the JSON functionality as a standalone 
+ *        utility without the need to externalize the world.
+ *
+ *  See the externalized header in doc/externalize for details.
+ */
+#include "DD4hep/Printout.h"
+
+#endif   /* DD4HEP_DDCORE_JSON_PRINTOUT_H  */
---
+++ DDCore/include/JSON/config.h
@@ -0,0 +1,35 @@
+//==========================================================================
+//  AIDA Detector description implementation for LCD
+//--------------------------------------------------------------------------
+// Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
+// All rights reserved.
+//
+// For the licensing terms see $DD4hepINSTALL/LICENSE.
+// For the list of contributors see $DD4hepINSTALL/doc/CREDITS.
+//
+// Author     : M.Frank
+//
+//==========================================================================
+#ifndef DD4HEP_DDCORE_JSON_CONFIG_H
+#define DD4HEP_DDCORE_JSON_CONFIG_H
+
+#define DD4HEP_USE_BOOST_JSON 1
+
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+
+/// Namespace for the AIDA detector description toolkit
+namespace DD4hep {
+
+  /// Namespace for the AIDA detector description toolkit supporting JSON utilities
+  namespace JSON {
+
+    typedef char JsonChar;
+    typedef boost::property_tree::ptree             ptree;
+    typedef boost::property_tree::ptree             JsonDocument;
+    typedef boost::property_tree::ptree::value_type JsonAttr;
+    typedef boost::property_tree::ptree::value_type JsonElement;
+  }       /* End namespace JSON              */
+}         /* End namespace DD4hep            */
+
+#endif   /* DD4HEP_DDCORE_JSON_CONFIG_H  */
