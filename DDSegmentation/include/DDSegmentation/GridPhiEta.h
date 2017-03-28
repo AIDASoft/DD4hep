ce5d55fbdcb834c01481356f27dd1604597b9507
Joschka Lingemann <joschka.lingemann@cern.ch>
2017-03-28 16:30:08 +0200

    Add segmentations in Phi-Eta and R-Phi-Eta

---
+++ DDSegmentation/include/DDSegmentation/GridPhiEta.h
@@ -0,0 +1,152 @@
+#ifndef DETSEGMENTATION_GRIDPHIETA_H
+#define DETSEGMENTATION_GRIDPHIETA_H
+
+#include "DDSegmentation/Segmentation.h"
+
+/* #include "DDSegmentation/SegmentationUtil.h" */
+#include "TVector3.h"
+#include <cmath>
+
+/** GridPhiEta DDSegmentation/include/DDSegmentation/GridPhiEta.h GridPhiEta.h
+ *
+ *  Segmentation in eta and phi.
+ *
+ *  @author    Anna Zaborowska
+ */
+
+namespace DD4hep {
+namespace DDSegmentation {
+class GridPhiEta : public Segmentation {
+public:
+  /// default constructor using an arbitrary type
+  GridPhiEta(const std::string& aCellEncoding);
+  /// Default constructor used by derived classes passing an existing decoder
+  GridPhiEta(BitField64* aDecoder);
+
+  /// destructor
+  virtual ~GridPhiEta() = default;
+
+  /**  Determine the global position based on the cell ID.
+   *   @warning This segmentation has no knowledge of radius, so radius = 1 is taken into calculations.
+   *   @param[in] aCellId ID of a cell.
+   *   return Position (radius = 1).
+   */
+  virtual Vector3D position(const CellID& aCellID) const;
+  /**  Determine the cell ID based on the position.
+   *   @param[in] aLocalPosition (not used).
+   *   @param[in] aGlobalPosition position in the global coordinates.
+   *   @param[in] aVolumeId ID of a volume.
+   *   return Cell ID.
+   */
+  virtual CellID cellID(const Vector3D& aLocalPosition, const Vector3D& aGlobalPosition, const VolumeID& aVolumeID) const;
+  /**  Determine the pseudorapidity based on the cell ID.
+   *   @param[in] aCellId ID of a cell.
+   *   return Pseudorapidity.
+   */
+  double eta(const CellID& aCellID) const;
+  /**  Determine the azimuthal angle based on the cell ID.
+   *   @param[in] aCellId ID of a cell.
+   *   return Phi.
+   */
+  double phi(const CellID& aCellID) const;
+  /**  Get the grid size in pseudorapidity.
+   *   return Grid size in eta.
+   */
+  inline double gridSizeEta() const {
+    return m_gridSizeEta;
+  }
+  /**  Get the grid size in phi.
+   *   return Grid size in phi.
+   */
+  inline double gridSizePhi() const {
+    return 2 * M_PI / static_cast<double>(m_phiBins);
+  }
+  /**  Get the number of bins in azimuthal angle.
+   *   return Number of bins in phi.
+   */
+  inline int phiBins() const {
+    return m_phiBins;
+  }
+  /**  Get the coordinate offset in pseudorapidity.
+   *   return The offset in eta.
+   */
+  inline double offsetEta() const {
+    return m_offsetEta;
+  }
+  /**  Get the coordinate offset in azimuthal angle.
+   *   return The offset in phi.
+   */
+  inline double offsetPhi() const {
+    return m_offsetPhi;
+  }
+  /**  Get the field name used for pseudorapidity
+   *   return The field name for eta.
+   */
+  inline const std::string& fieldNameEta() const {
+    return m_etaID;
+  }
+  /**  Get the field name for azimuthal angle.
+   *   return The field name for phi.
+   */
+  inline const std::string& fieldNamePhi() const {
+    return m_phiID;
+  }
+  /**  Set the grid size in pseudorapidity.
+   *   @param[in] aCellSize Cell size in eta.
+   */
+  void setGridSizeEta(double aCellSize) {
+    m_gridSizeEta = aCellSize;
+  }
+  /**  Set the number of bins in azimuthal angle.
+   *   @param[in] aNumberBins Number of bins in phi.
+   */
+  inline void setPhiBins(int bins) {
+    m_phiBins = bins;
+  }
+  /**  Set the coordinate offset in pseudorapidity.
+   *   @param[in] aOffset Offset in eta.
+   */
+  inline void setOffsetEta(double offset) {
+    m_offsetEta = offset;
+  }
+  /**  Set the coordinate offset in azimuthal angle.
+   *   @param[in] aOffset Offset in phi.
+   */
+  inline void setOffsetPhi(double offset) {
+    m_offsetPhi = offset;
+  }
+  /**  Set the field name used for pseudorapidity.
+   *   @param[in] aFieldName Field name for eta.
+   */
+  inline void setFieldNameEta(const std::string& fieldName) {
+    m_etaID = fieldName;
+  }
+  /**  Set the field name used for azimuthal angle.
+   *   @param[in] aFieldName Field name for phi.
+   */
+  inline void setFieldNamePhi(const std::string& fieldName) {
+    m_phiID = fieldName;
+  }
+
+protected:
+  /// determine the pseudorapidity based on the current cell ID
+  double eta() const;
+  /// determine the azimuthal angle phi based on the current cell ID
+  double phi() const;
+
+  /// the grid size in eta
+  double m_gridSizeEta;
+  /// the number of bins in phi
+  int m_phiBins;
+  /// the coordinate offset in eta
+  double m_offsetEta;
+  /// the coordinate offset in phi
+  double m_offsetPhi;
+  /// the field name used for eta
+  std::string m_etaID;
+  /// the field name used for phi
+  std::string m_phiID;
+};
+}
+}
+#endif /* DETSEGMENTATION_GRIDPHIETA_H */
---
+++ DDSegmentation/include/DDSegmentation/GridRPhiEta.h
@@ -0,0 +1,97 @@
+#ifndef DETSEGMENTATION_GRIDRPHIETA_H
+#define DETSEGMENTATION_GRIDRPHIETA_H
+
+// Framework includes
+#include "DDSegmentation/GridPhiEta.h"
+
+/** GridRPhiEta DDSegmentation/include/DDSegmentation/GridRPhiEta.h GridRPhiEta.h
+ *
+ *  Segmentation in R, eta and phi.
+ *  Based on GridPhiEta, addition of radial coordinate.
+ *  This segmentation returns global position of the cell based on the cellID.
+ *
+ *  @author    Anna Zaborowska
+ */
+
+namespace DD4hep {
+namespace DDSegmentation {
+class GridRPhiEta: public GridPhiEta {
+public:
+  /// default constructor using an arbitrary type
+  GridRPhiEta(const std::string& aCellEncoding);
+
+  /// Default constructor used by derived classes passing an existing decoder
+  GridRPhiEta(BitField64* aDecoder);
+
+  /// destructor
+  virtual ~GridRPhiEta() = default;
+
+  /**  Determine the global position based on the cell ID.
+   *   @param[in] aCellId ID of a cell.
+   *   return Position.
+   */
+  virtual Vector3D position(const CellID& aCellID) const;
+  /**  Determine the cell ID based on the position.
+   *   @param[in] aLocalPosition (not used).
+   *   @param[in] aGlobalPosition position in the global coordinates.
+   *   @param[in] aVolumeId ID of a volume.
+   *   return Cell ID.
+   */
+  virtual CellID cellID(const Vector3D& aLocalPosition, const Vector3D& aGlobalPosition, const VolumeID& aVolumeID) const;
+  /**  Determine the radius based on the cell ID.
+   *   @param[in] aCellId ID of a cell.
+   *   return Radius.
+   */
+  double r(const CellID& aCellID) const;
+  /**  Get the grid size in radial distance from the detector centre.
+   *   return Grid size in radial distance.
+   */
+  inline double gridSizeR() const {
+    return m_gridSizeR;
+  }
+  /**  Get the coordinate offset in radial distance.
+   *   return The offset in R.
+   */
+  inline double offsetR() const {
+    return m_offsetR;
+  }
+  /**  Get the field name for radial distance.
+   *   return The field name for radial distance.
+   */
+  inline const std::string& fieldNameR() const {
+    return m_rID;
+  }
+  /**  Set the grid size in radial distance.
+   *   @param[in] aCellSize Cell size in radial distance.
+   */
+  void setGridSizeR(double aCellSize) {
+    m_gridSizeR = aCellSize;
+  }
+  /**  Set the coordinate offset in radial distance.
+   *   @param[in] aOffset Offset in radial distance.
+   */
+  inline void setOffsetR(double offset) {
+    m_offsetR = offset;
+  }
+  /**  Set the field name used for radial distance.
+   *   @param[in] aFieldName Field name for R.
+   */
+  inline void setFieldNameR(const std::string& fieldName) {
+    m_rID = fieldName;
+  }
+
+private:
+  /// determine the radial distance R based on the current cell ID
+  double r() const;
+
+  /// the grid size in r
+  double m_gridSizeR;
+  /// the coordinate offset in r
+  double m_offsetR;
+  /// the field name used for r
+  std::string m_rID;
+
+};
+}
+}
+#endif /* DETSEGMENTATION_GRIDRPHIETA_H */
