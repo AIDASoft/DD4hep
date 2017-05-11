a2d606e896a8afffd4234c0bd9998c8fa88643c3
Marko Petric <marko.petric@cern.ch>
2017-05-11 11:19:59 +0200

    Use separate init file for mac

---
+++ .dd4hep-ci.d/init_mac.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+#Determine which OS you are using
+if [ "$(uname)" == "Darwin" ]; then
+if [ $(sw_vers -productVersion | awk -F '.' '{print $1 "." $2}') == "10.12" ]; then
+OS=mac1012
+COMPILER_TYPE=clang
+COMPILER_VERSION=clang80
+else
+echo "Bootstrap only works on macOS Sierra (10.12)"
+fi
+elif [ "$(uname)" == "Linux" ]; then
+if [ "$( cat /etc/*-release | grep Scientific )" ]; then
+OS=slc6
+elif [ "$( cat /etc/*-release | grep CentOS )" ]; then
+OS=centos7
+fi
+else
+echo "UNKNOWN OS"
+exit 1
+fi
+
+#Determine is you have CVMFS installed
+if [ ! -d "/cvmfs" ]; then
+echo "No CVMFS detected, please install it."
+exit 1
+fi
+
+if [ ! -d "/cvmfs/clicdp.cern.ch" ]; then
+echo "No clicdp CVMFS repository detected, please add it."
+exit 1
+fi
+
+
+#Determine which compiler to use
+if [ -z ${COMPILER_TYPE} ]; then
+COMPILER_TYPE="gcc"
+fi
+if [ ${COMPILER_TYPE} == "gcc" ]; then
+COMPILER_VERSION="gcc62"
+fi
+if [ ${COMPILER_TYPE} == "llvm" ]; then
+COMPILER_VERSION="llvm39"
+fi
+
+
+#Choose build type
+if [ -z ${BUILD_TYPE} ]; then
+BUILD_TYPE=opt
+fi
+
+
+# General variables
+CLICREPO=/cvmfs/clicdp.cern.ch
+BUILD_FLAVOUR=x86_64-${OS}-${COMPILER_VERSION}-${BUILD_TYPE}
+
+#--------------------------------------------------------------------------------
+#     Compiler
+#--------------------------------------------------------------------------------
+
+if [ ${COMPILER_TYPE} == "gcc" ]; then
+source ${CLICREPO}/compilers/gcc/6.2.0/x86_64-${OS}/setup.sh
+fi
+if [ ${COMPILER_TYPE} == "llvm" ]; then
+source ${CLICREPO}/compilers/llvm/3.9.0/x86_64-${OS}/setup.sh
+fi
+
+#--------------------------------------------------------------------------------
+#     CMake
+#--------------------------------------------------------------------------------
+
+export CMAKE_HOME=${CLICREPO}/software/CMake/3.6.2/${BUILD_FLAVOUR}
+export PATH=${CMAKE_HOME}/bin:$PATH
+
+#--------------------------------------------------------------------------------
+#     Python
+#--------------------------------------------------------------------------------
+
+export PYTHONDIR=${CLICREPO}/software/Python/2.7.12/${BUILD_FLAVOUR}
+export PATH=${PYTHONDIR}/bin:$PATH
+export LD_LIBRARY_PATH=${PYTHONDIR}/lib:${LD_LIBRARY_PATH}
+
+#--------------------------------------------------------------------------------
+#     ROOT
+#--------------------------------------------------------------------------------
+
+export ROOTSYS=${CLICREPO}/software/ROOT/6.08.00/${BUILD_FLAVOUR}
+export PYTHONPATH="$ROOTSYS/lib:$PYTHONPATH"
+export PATH="$ROOTSYS/bin:$PATH"
+export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
+
+#--------------------------------------------------------------------------------
+#     XercesC
+#--------------------------------------------------------------------------------
+
+export XercesC_HOME=${CLICREPO}/software/Xerces-C/3.1.4/${BUILD_FLAVOUR}
+export PATH="$XercesC_HOME/bin:$PATH"
+export LD_LIBRARY_PATH="$XercesC_HOME/lib:$LD_LIBRARY_PATH"
+
+
+#--------------------------------------------------------------------------------
+#     Geant4
+#--------------------------------------------------------------------------------
+
+export G4INSTALL=${CLICREPO}/software/Geant4/10.02.p02/${BUILD_FLAVOUR}
+export G4ENV_INIT="${G4INSTALL}/bin/geant4.sh"
+export G4SYSTEM="Linux-g++"
+
+
+#--------------------------------------------------------------------------------
+#     LCIO
+#--------------------------------------------------------------------------------
+export LCIO=${CLICREPO}/software/LCIO/2.7.3/${BUILD_FLAVOUR}
+export PYTHONPATH=${LCIO}/python
+export PATH=${LCIO}/bin:$PATH
+export LD_LIBRARY_PATH="$LCIO/lib:$LD_LIBRARY_PATH"
+
+
+#--------------------------------------------------------------------------------
+#     Boost
+#--------------------------------------------------------------------------------
+
+export BOOST_ROOT=${CLICREPO}/software/Boost/1.62.0/${BUILD_FLAVOUR}
+export LD_LIBRARY_PATH="${BOOST_ROOT}/lib:$LD_LIBRARY_PATH"
+
+#--------------------------------------------------------------------------------
+#     Ninja
+#--------------------------------------------------------------------------------
+
+export Ninja_HOME=${CLICREPO}/software/Ninja/1.7.1/${BUILD_FLAVOUR}
+export PATH="$Ninja_HOME:$PATH"
