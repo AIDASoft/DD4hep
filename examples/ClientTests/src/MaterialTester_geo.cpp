d0a1d36cb1b0fe26b0bfe6732138854148a0a4e3
Markus Frank <Markus.Frank@cern.ch>
2017-06-17 04:58:55 +0200

    Externalize parsers to external package DDParsers

--- examples/ClientTests/src/MaterialTester_geo.cpp
+++ examples/ClientTests/src/MaterialTester_geo.cpp
@@ -1,11 +1,16 @@
-// $Id: ILDExVXD_geo.cpp 673 2013-08-05 10:01:33Z gaede $
-//====================================================================
+//==========================================================================
 //  AIDA Detector description implementation 
-//--------------------------------------------------------------------
+//--------------------------------------------------------------------------
+// Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
+// All rights reserved.
 //
-//  Author     : M.Frank
+// For the licensing terms see $DD4hepINSTALL/LICENSE.
+// For the list of contributors see $DD4hepINSTALL/doc/CREDITS.
 //
-//====================================================================
+// Author     : M.Frank
+//
+//==========================================================================
+
 // Framework include files
 #include "DD4hep/DetFactoryHelper.h"
 #include "DD4hep/Printout.h"
@@ -26,26 +31,28 @@ static Ref_t create_element(Detector& description, xml_h xml_det, SensitiveDetec
   for(xml_coll_t k(x_det,_Unicode(test)); k; ++k)  {	
     xml_comp_t c = k;
     Material mat = description.material(c.nameStr());
-    TGeoMaterial* m = mat->GetMaterial();
+    TGeoMaterial* material = mat->GetMaterial();
     printout(INFO,det_name,"+++ Material:%s [%p, %p] Z=%6.2f A=%6.2f D=%9.4f [g/cm3]",
-	     m->GetName(), mat.ptr(), m, m->GetZ(), m->GetA(), m->GetDensity());
+             material->GetName(), mat.ptr(), m, material->GetZ(),
+             material->GetA(), material->GetDensity());
     printout(INFO,det_name,"+++          Radiation Length:%9.4f Interaction length:%9.4f Mixture:%s",
-	     m->GetRadLen(), m->GetIntLen(), yes_no(m->IsMixture()));
+             material->GetRadLen(), material->GetIntLen(),
+             yes_no(material->IsMixture()));
     printout(INFO,det_name,"+++          Elements:%d Index:%d",
-	     m->GetNelements(), m->GetIndex());
-    for(Int_t i=0, n=m->GetNelements(); i<n; ++i)  {
-      TGeoElement* e = m->GetElement(i);
+             material->GetNelements(), material->GetIndex());
+    for(Int_t i=0, n=material->GetNelements(); i<n; ++i)  {
+      TGeoElement* e = material->GetElement(i);
       Double_t a=0., z=0., w=0.;
-      m->GetElementProp(a,z,w,i);
+      material->GetElementProp(a,z,w,i);
       printout(INFO,det_name,"+++          ELT[%02d]: %s Z=%3d N=%3d N_eff=%7.2f A=%6.2f Weight=%9.4f ",
-	       i, e->GetName(), e->Z(), e->N(), e->Neff(), e->A(), w);
-      if ( m->IsMixture() )   {
-	TGeoMixture* mix = (TGeoMixture*)m;
-	Int_t* nmix = mix->GetNmixt();
-	Double_t* wmix = mix->GetWmixt();
-	printout(INFO,det_name,"+++                   Zmix:%7.3f Nmix:%3d Amix:%7.3f Wmix:%7.3f",
-		 mix->GetZmixt()[i],nmix ? nmix[i] : -1,
-		 mix->GetAmixt()[i],wmix ? wmix[i] : -1e0);
+               i, e->GetName(), e->Z(), e->N(), e->Neff(), e->A(), w);
+      if ( material->IsMixture() )   {
+        TGeoMixture* mix = (TGeoMixture*)m;
+        Int_t* nmix = mix->GetNmixt();
+        Double_t* wmix = mix->GetWmixt();
+        printout(INFO,det_name,"+++                   Zmix:%7.3f Nmix:%3d Amix:%7.3f Wmix:%7.3f",
+                 mix->GetZmixt()[i],nmix ? nmix[i] : -1,
+                 mix->GetAmixt()[i],wmix ? wmix[i] : -1e0);
       }
     }
   }
