d08a4bb20d85239f6a1e1a4f017cfe685bb574df
Frank Gaede <frank.gaede@desy.de>
2017-09-28 22:22:23 +0200

    add missing BitFieldCoder.h

---
+++ DDCore/include/DD4hep/BitFieldCoder.h
@@ -0,0 +1,30 @@
+//==========================================================================
+//  AIDA Detector description implementation 
+//--------------------------------------------------------------------------
+// Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
+// All rights reserved.
+//
+// For the licensing terms see $DD4hepINSTALL/LICENSE.
+// For the list of contributors see $DD4hepINSTALL/doc/CREDITS.
+//
+// Author     : M.Frank
+//
+//==========================================================================
+#ifndef DD4HEP_DDCORE_BITFIELD64_H
+#define DD4HEP_DDCORE_BITFIELD64_H
+
+// Framework include files
+#include "DDSegmentation/BitFieldCoder.h"
+
+/// Namespace for the AIDA detector description toolkit
+namespace dd4hep {
+
+  /// Namespace for implementation details of the AIDA detector description toolkit
+  namespace detail {
+
+    using DDSegmentation::BitFieldCoder;
+    using DDSegmentation::BitFieldElement;
+
+  }       /* End namespace detail           */
+}         /* End namespace dd4hep             */
+#endif    /* DD4HEP_DDCORE_BITFIELD64_H     */
