377b048bb66c83ba2000db73a2b722ba29e6425a
Marko Petric <marko.petric@cern.ch>
2017-12-01 14:50:27 +0100

    Improve DDSim defaults

--- DDSim/Helper/MagneticField.py
+++ DDSim/Helper/MagneticField.py
@@ -6,12 +6,12 @@ class MagneticField( ConfigHelper ):
   """Configuration for the magnetic field (stepper)"""
   def __init__( self ):
     super(MagneticField, self).__init__()
-    self.stepper = "HelixSimpleRunge"
+    self.stepper = "G4ClassicalRK4"
     self.equation = "Mag_UsualEqRhs"
     self.eps_min = 5e-05*mm
     self.eps_max = 0.001*mm
     self.min_chord_step = 0.01*mm
     self.delta_chord = 0.25*mm
-    self.delta_intersection = 1e-05*mm
-    self.delta_one_step = 1e-04*mm
+    self.delta_intersection = 0.001*mm
+    self.delta_one_step = 0.01*mm
     self.largest_step = 10*m
