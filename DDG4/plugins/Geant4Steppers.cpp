c99a3fb9689883c949a768843118ac27a87fc6aa
Markus Frank <Markus.Frank@cern.ch>
2017-12-01 20:25:53 +0100

    Update DDG4 stepper factories

--- DDG4/plugins/Geant4Steppers.cpp
+++ DDG4/plugins/Geant4Steppers.cpp
@@ -15,6 +15,7 @@
 #include "DDG4/Factories.h"
 
 // Geant 4 include files
+#include "G4Version.hh"
 #include "G4ExplicitEuler.hh"
 #include "G4ImplicitEuler.hh"
 #include "G4SimpleRunge.hh"
@@ -22,10 +23,6 @@
 #include "G4ClassicalRK4.hh"
 #include "G4ConstRK4.hh"
 #include "G4CashKarpRKF45.hh"
-#include "G4DormandPrince745.hh"
-#include "G4DormandPrinceRK56.hh"
-#include "G4DormandPrinceRK78.hh"
-#include "G4BogackiShampine23.hh"
 
 DECLARE_GEANT4_STEPPER(ImplicitEuler)
 DECLARE_GEANT4_STEPPER(ExplicitEuler)
@@ -34,19 +31,24 @@ DECLARE_GEANT4_STEPPER(SimpleHeum)
 DECLARE_GEANT4_MAGSTEPPER(ConstRK4)
 DECLARE_GEANT4_STEPPER(ClassicalRK4)
 DECLARE_GEANT4_STEPPER(CashKarpRKF45)
+
+#if G4VERSION_NUMBER >=1030
+#include "G4DoLoMcPriRK34.hh"
+#include "G4DormandPrince745.hh"
+#include "G4DormandPrinceRK56.hh"
+#include "G4DormandPrinceRK78.hh"
+#include "G4BogackiShampine23.hh"
+#include "G4BogackiShampine45.hh"
+DECLARE_GEANT4_STEPPER(DoLoMcPriRK34)
 DECLARE_GEANT4_STEPPER(DormandPrince745)
 DECLARE_GEANT4_STEPPER(DormandPrinceRK56)
 DECLARE_GEANT4_STEPPER(DormandPrinceRK78)
 DECLARE_GEANT4_STEPPER(BogackiShampine23)
-
-#if G4VERSION_NUMBER>=1003
-#include "G4DoLoMcPriRK34.hh"
-#include "G4BogackiShampine45.hh"
 DECLARE_GEANT4_STEPPER(BogackiShampine45)
-DECLARE_GEANT4_STEPPER(DoLoMcPriRK34)
 #endif
 
 // Geant 4 include files
+#include "G4MagHelicalStepper.hh"
 #include "G4HelixExplicitEuler.hh"
 #include "G4HelixImplicitEuler.hh"
 #include "G4HelixMixedStepper.hh"
@@ -59,6 +61,12 @@ DECLARE_GEANT4_MAGSTEPPER(HelixMixedStepper)
 DECLARE_GEANT4_MAGSTEPPER(HelixSimpleRunge)
 DECLARE_GEANT4_MAGSTEPPER(HelixHeum)
 
+#if G4VERSION_NUMBER>=1020
+#include "G4NystromRK4.hh"
+#include "G4RKG3_Stepper.hh"
+DECLARE_GEANT4_MAGSTEPPER(RKG3_Stepper)
+DECLARE_GEANT4_MAGSTEPPER(NystromRK4)
+#endif
 
 // Geant 4 include files
 #include "G4Mag_EqRhs.hh"
@@ -71,3 +79,20 @@ DECLARE_GEANT4_MAGMOTION(Mag_SpinEqRhs)
 DECLARE_GEANT4_MAGMOTION(Mag_UsualEqRhs)
 //DECLARE_GEANT4_MAGMOTION(KM_OpticalEqRhs);
 //DECLARE_GEANT4_MAGMOTION(KM_NucleonEqRhs);
+
+#if 0 // G4VERSION_NUMBER>=1002
+// Undefined factories !!!
+#include "G4MonopoleEq.hh"
+#include "G4EqEMFieldWithEDM.hh"
+#include "G4EqEMFieldWithSpin.hh"
+#include "G4EqGravityField.hh"
+#include "G4EqMagElectricField.hh"
+
+// G4EquationOfMotion*(G4UniformGravityField*)
+DECLARE_GEANT4_MAGMOTION(EqGravityField)
+// G4EquationOfMotion*(G4ElectroMagneticField*)
+DECLARE_GEANT4_MAGMOTION(MonopoleEq)
+DECLARE_GEANT4_MAGMOTION(EqMagElectricField)
+DECLARE_GEANT4_MAGMOTION(EqEMFieldWithEDM)
+DECLARE_GEANT4_MAGMOTION(EqEMFieldWithSpin)
+#endif
