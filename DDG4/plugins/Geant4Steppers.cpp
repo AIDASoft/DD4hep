f01608fadd76fdd128dafc9e93c31e1b83b46e74
Markus Frank <Markus.Frank@cern.ch>
2017-12-01 19:50:15 +0100

    Update DDG4 stepper factories

--- DDG4/plugins/Geant4Steppers.cpp
+++ DDG4/plugins/Geant4Steppers.cpp
@@ -22,7 +22,6 @@
 #include "G4ClassicalRK4.hh"
 #include "G4ConstRK4.hh"
 #include "G4CashKarpRKF45.hh"
-#include "G4DoLoMcPriRK34.hh"
 #include "G4DormandPrince745.hh"
 #include "G4DormandPrinceRK56.hh"
 #include "G4DormandPrinceRK78.hh"
@@ -35,15 +34,16 @@ DECLARE_GEANT4_STEPPER(SimpleHeum)
 DECLARE_GEANT4_MAGSTEPPER(ConstRK4)
 DECLARE_GEANT4_STEPPER(ClassicalRK4)
 DECLARE_GEANT4_STEPPER(CashKarpRKF45)
-DECLARE_GEANT4_STEPPER(DoLoMcPriRK34)
 DECLARE_GEANT4_STEPPER(DormandPrince745)
 DECLARE_GEANT4_STEPPER(DormandPrinceRK56)
 DECLARE_GEANT4_STEPPER(DormandPrinceRK78)
 DECLARE_GEANT4_STEPPER(BogackiShampine23)
 
 #if G4VERSION_NUMBER>=1003
+#include "G4DoLoMcPriRK34.hh"
 #include "G4BogackiShampine45.hh"
 DECLARE_GEANT4_STEPPER(BogackiShampine45)
+DECLARE_GEANT4_STEPPER(DoLoMcPriRK34)
 #endif
 
 // Geant 4 include files
