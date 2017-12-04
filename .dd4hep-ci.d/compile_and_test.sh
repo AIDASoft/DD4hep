520ba711c9109248b667f8cb52f30a2fd2a16f70
Marko Petric <marko.petric@cern.ch>
2017-12-04 15:33:55 +0100

    Update CI to GG7 and LLVM5

--- .dd4hep-ci.d/compile_and_test.sh
+++ .dd4hep-ci.d/compile_and_test.sh
@@ -5,7 +5,7 @@ source /DD4hep/.dd4hep-ci.d/init_x86_64.sh
 cd /DD4hep
 mkdir build
 cd build
-cmake -GNinja -D DD4HEP_USE_GEANT4=ON -DBoost_NO_BOOST_CMAKE=ON -D DD4HEP_USE_LCIO=ON -D BUILD_TESTING=ON  -D Geant4_DIR=$G4INSTALL/lib64/Geant4-10.2.3 -D DD4HEP_USE_CXX11=ON -DCMAKE_BUILD_TYPE=Release -DROOT_DIR=$ROOTSYS -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
+cmake -GNinja -D DD4HEP_USE_GEANT4=ON -DBoost_NO_BOOST_CMAKE=ON -D DD4HEP_USE_LCIO=ON -D BUILD_TESTING=ON  -D Geant4_DIR=$G4INSTALL/lib64/Geant4-10.3.3 -D DD4HEP_USE_CXX11=ON -DCMAKE_BUILD_TYPE=Release -DROOT_DIR=$ROOTSYS -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always" .. && \
 ninja && \
 ninja install && \
 . ../bin/thisdd4hep.sh && \
--- .dd4hep-ci.d/init_x86_64.sh
+++ .dd4hep-ci.d/init_x86_64.sh
@@ -2,12 +2,12 @@
 
 #Determine which OS you are using
 if [ "$(uname)" == "Darwin" ]; then
-    if [ $(sw_vers -productVersion | awk -F '.' '{print $1 "." $2}') == "10.12" ]; then
-        OS=mac1012
-        COMPILER_TYPE=clang
-        COMPILER_VERSION=clang80
+    if [ $(sw_vers -productVersion | awk -F '.' '{print $1 "." $2}') == "10.13" ]; then
+        OS=mac1013
+        COMPILER_TYPE=llvm
+        COMPILER_VERSION=llvm90
     else
-        echo "Bootstrap only works on macOS Sierra (10.12)"
+        echo "Bootstrap only works on macOS High Sierra (10.13)"
     fi
 elif [ "$(uname)" == "Linux" ]; then
     if [ "$( cat /etc/*-release | grep Scientific )" ]; then
@@ -39,8 +39,8 @@ fi
 if [ ${COMPILER_TYPE} == "gcc" ]; then
     COMPILER_VERSION="gcc7"
 fi
-if [ ${COMPILER_TYPE} == "llvm" ]; then
-    COMPILER_VERSION="llvm40"
+if [[ ${COMPILER_TYPE} == "llvm" && ${OS} != "mac1013" ]]; then
+    COMPILER_VERSION="llvm5"
 fi
 
 
@@ -58,25 +58,25 @@ BUILD_FLAVOUR=x86_64-${OS}-${COMPILER_VERSION}-${BUILD_TYPE}
 #     Compiler
 #--------------------------------------------------------------------------------
 
-if [ ${COMPILER_TYPE} == "gcc" ]; then
-    source ${CLICREPO}/compilers/gcc/7.1.0/x86_64-${OS}/setup.sh
+if [[ ${COMPILER_TYPE} == "gcc" && ${OS} != "mac1013" ]]; then
+    source ${CLICREPO}/compilers/gcc/7.2.0/x86_64-${OS}/setup.sh
 fi
-if [ ${COMPILER_TYPE} == "llvm" ]; then
-    source ${CLICREPO}/compilers/llvm/4.0.0/x86_64-${OS}/setup.sh
+if [[ ${COMPILER_TYPE} == "llvm" && ${OS} != "mac1013" ]]; then
+    source ${CLICREPO}/compilers/llvm/5.0.0/x86_64-${OS}/setup.sh
 fi
 
 #--------------------------------------------------------------------------------
 #     CMake
 #--------------------------------------------------------------------------------
 
-export CMAKE_HOME=${CLICREPO}/software/CMake/3.8.1/${BUILD_FLAVOUR}
+export CMAKE_HOME=${CLICREPO}/software/CMake/3.9.5/${BUILD_FLAVOUR}
 export PATH=${CMAKE_HOME}/bin:$PATH
 
 #--------------------------------------------------------------------------------
 #     Python
 #--------------------------------------------------------------------------------
 
-export PYTHONDIR=${CLICREPO}/software/Python/2.7.13/${BUILD_FLAVOUR}
+export PYTHONDIR=${CLICREPO}/software/Python/2.7.14/${BUILD_FLAVOUR}
 export PATH=${PYTHONDIR}/bin:$PATH
 export LD_LIBRARY_PATH=${PYTHONDIR}/lib:${LD_LIBRARY_PATH}
 
@@ -84,7 +84,7 @@ export LD_LIBRARY_PATH=${PYTHONDIR}/lib:${LD_LIBRARY_PATH}
 #     ROOT
 #--------------------------------------------------------------------------------
 
-export ROOTSYS=${CLICREPO}/software/ROOT/6.08.06/${BUILD_FLAVOUR}
+export ROOTSYS=${CLICREPO}/software/ROOT/6.10.08/${BUILD_FLAVOUR}
 export PYTHONPATH="$ROOTSYS/lib:$PYTHONPATH"
 export PATH="$ROOTSYS/bin:$PATH"
 export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
@@ -93,7 +93,7 @@ export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
 #     XercesC
 #--------------------------------------------------------------------------------
 
-export XercesC_HOME=${CLICREPO}/software/Xerces-C/3.1.4/${BUILD_FLAVOUR}
+export XercesC_HOME=${CLICREPO}/software/Xerces-C/3.2.0/${BUILD_FLAVOUR}
 export PATH="$XercesC_HOME/bin:$PATH"
 export LD_LIBRARY_PATH="$XercesC_HOME/lib:$LD_LIBRARY_PATH"
 
@@ -103,7 +103,7 @@ export LD_LIBRARY_PATH="$XercesC_HOME/lib:$LD_LIBRARY_PATH"
 #--------------------------------------------------------------------------------
 #Determine which Geant4 version to use
 if [ -z ${GEANT4_VERSION} ]; then
-GEANT4_VERSION="10.02.p03"
+GEANT4_VERSION="10.03.p03"
 fi
 
 export G4INSTALL=${CLICREPO}/software/Geant4/${GEANT4_VERSION}/${BUILD_FLAVOUR}
@@ -114,7 +114,7 @@ export G4SYSTEM="Linux-g++"
 #--------------------------------------------------------------------------------
 #     LCIO
 #--------------------------------------------------------------------------------
-export LCIO=${CLICREPO}/software/LCIO/2.8.0/${BUILD_FLAVOUR}
+export LCIO=${CLICREPO}/software/LCIO/2.11.0/${BUILD_FLAVOUR}
 export PYTHONPATH=${LCIO}/python
 export PATH=${LCIO}/bin:$PATH
 export LD_LIBRARY_PATH="$LCIO/lib:$LD_LIBRARY_PATH"
@@ -124,14 +124,14 @@ export LD_LIBRARY_PATH="$LCIO/lib:$LD_LIBRARY_PATH"
 #     Boost
 #--------------------------------------------------------------------------------
 
-export BOOST_ROOT=${CLICREPO}/software/Boost/1.64.0/${BUILD_FLAVOUR}
+export BOOST_ROOT=${CLICREPO}/software/Boost/1.65.1/${BUILD_FLAVOUR}
 export LD_LIBRARY_PATH="${BOOST_ROOT}/lib:$LD_LIBRARY_PATH"
 
 #--------------------------------------------------------------------------------
 #     Ninja
 #--------------------------------------------------------------------------------
 
-export Ninja_HOME=${CLICREPO}/software/Ninja/1.7.2/${BUILD_FLAVOUR}
+export Ninja_HOME=${CLICREPO}/software/Ninja/1.8.2/${BUILD_FLAVOUR}
 export PATH="$Ninja_HOME:$PATH"
 
 #--------------------------------------------------------------------------------
