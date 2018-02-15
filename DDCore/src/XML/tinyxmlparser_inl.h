d5639c51e13f764c850a9b3a6e90e8e2e8d7c136
Marko Petric <marko.petric@cern.ch>
2018-02-15 11:01:26 +0100

    Add sequence for fallthrough to disable warning

--- DDCore/src/XML/tinyxmlparser_inl.h
+++ DDCore/src/XML/tinyxmlparser_inl.h
@@ -119,21 +119,21 @@ void TiXmlBase::ConvertUTF32ToUTF8( unsigned long input, char* output, int* leng
     --output;
     *output = (char)((input | BYTE_MARK) & BYTE_MASK);
     input >>= 6;
-    // fall through  [[fallthrough]];
+    [[fallthrough]];
   case 3:
     --output;
     *output = (char)((input | BYTE_MARK) & BYTE_MASK);
     input >>= 6;
-    // fall through  [[fallthrough]];
+    [[fallthrough]];
   case 2:
     --output;
     *output = (char)((input | BYTE_MARK) & BYTE_MASK);
     input >>= 6;
-    // fall through  [[fallthrough]];
+    [[fallthrough]];
   case 1:
     --output;
     *output = (char)(input | FIRST_BYTE_MARK[*length]);
-    // fall through  [[fallthrough]];
+    [[fallthrough]];
   default:
     break;
   }
