d66aacd8f1ff3865d0b5bd4cad4c1872b3474e38
Markus Frank <Markus.Frank@cern.ch>
2018-07-02 10:07:41 +0200

    Add CodexB standalone example

--- examples/DDCodex/src/CODEXb_geo.cpp
+++ examples/DDCodex/src/CODEXb_geo.cpp
@@ -34,19 +34,34 @@ static Ref_t create_element(Detector& description, xml_h e, Ref_t sens)  {
   double     sin_cone   = std::sin(std::atan(x_envelope.rmax()/env_dz/2.0));
   double     cos_beam   = std::cos(env_angle);
   double     sin_beam   = std::sin(env_angle);
-  Cone       env_cone(env_dz,0,0.0001,x_envelope.rmax(),x_envelope.rmax()+0.0001);
-  Volume     env_vol (det_name,env_cone,description.air());
+  //Cone       env_cone(env_dz,0,0.0001,x_envelope.rmax(),x_envelope.rmax()+0.0001);
+  //Volume     env_vol (det_name,env_cone,description.air());
+  // If the cone should be removed and replaced by an assembly,
+  // comment the upper to and enable the lower line
+  Assembly   env_vol(det_name);
   PlacedVolume pv;
+  SensitiveDetector sd = sens;
 
+  sd.setType("tracker");
+
+#if 0
+  Box b(2000,2000,2000);
+  Volume vb(det_name+"_bbb",b,description.material("Si"));
+  pv = env_vol.placeVolume(vb, Position(0,0,0));
+  vb.setSensitiveDetector(sd);
+  pv.addPhysVolID("type", 0);
+  pv.addPhysVolID("station", 20);
+#endif
+  
+  // Set visualization, limits and region (if present)
   env_vol.setRegion(description, x_det.regionStr());
   env_vol.setLimitSet(description, x_det.limitsStr());
   env_vol.setVisAttributes(description, x_det.visStr());
-  printout(INFO,"CODEX-b","%s Envelope: dz=%g r1=%g r2=%g beam-angle=%g atan(cone)=%g",
-           det_name.c_str(), env_cone->GetDz(), env_cone->GetRmin1(), env_cone->GetRmax2(),
-           env_angle, std::atan(x_envelope.rmax()/env_dz/2.0));
+  printout(INFO,"CODEX-b","%s beam-angle=%g atan(cone)=%g",
+           det_name.c_str(), env_angle, std::atan(x_envelope.rmax()/env_dz/2.0));
 
   Tube tub(0, 3, x_envelope.dz()+500);
-  Volume tub_vol("Tube", tub, description.air());
+  Volume tub_vol(det_name+"_Tube", tub, description.air());
   tub_vol.setVisAttributes(description, "BlackVis");
   pv = env_vol.placeVolume(tub_vol, Position(0,0,0));
 
@@ -55,7 +70,7 @@ static Ref_t create_element(Detector& description, xml_h e, Ref_t sens)  {
     xml_comp_t shield = i;
     double     z  = shield.z(), dz = shield.dz();
     double     r1 = sin_cone*z, r2 = sin_cone*(z+dz);
-    string     nam = shield.nameStr();
+    string     nam = det_name+"_"+shield.nameStr();
     Cone       con(dz/2., 0., r1, 0., r2);
     Material   mat(description.material(shield.attr<string>(_U(material))));
     Volume     vol(nam,con,mat);
@@ -66,8 +81,9 @@ static Ref_t create_element(Detector& description, xml_h e, Ref_t sens)  {
     pv = env_vol.placeVolume(vol, Position(0,0,-env_dz+z+dz/2.0));
     if ( shield.isSensitive() )   {
       DetElement det(sdet, "shield_"+nam, x_det.id());
+      vol.setSensitiveDetector(sd);
       pv.addPhysVolID("type", 0);
-      pv.addPhysVolID("module", num_sensitive);
+      pv.addPhysVolID("station", num_sensitive);
       det.setPlacement(pv);
       ++num_sensitive;
     }
@@ -93,13 +109,19 @@ static Ref_t create_element(Detector& description, xml_h e, Ref_t sens)  {
     if ( station.hasAttr(_U(z)) ) {
       z0 = station.z()+x_tot/2.0/cos_beam;
       x0 = -x_tot/2.0/sin_beam;
-    }    
+    }
+    // Set visualization, limits and region (if present)
     vol.setVisAttributes(description, station.visStr());
+    vol.setLimitSet(description,  station.limitsStr());
+    vol.setRegion(description, station.regionStr());
+    if ( station.isSensitive() )   {
+      vol.setSensitiveDetector(sd);
+    }
     for(int j=0; j < repeat; ++j)    {
       DetElement det(sdet, _toString(station_number,"module%d"), station_number);
       pv = env_vol.placeVolume(vol, Transform3D(RotationZYX(0,M_PI/2.0-env_angle,0), Position(x0,0,z0)));
       pv.addPhysVolID("type", 1);
-      pv.addPhysVolID("module", station_number);
+      pv.addPhysVolID("station", station_number);
       printout(INFO,"CODEX-b","%s Module: %2d %-12s x=%g y=%g z=%7g Dist:%g dx:%g dz:%g",
                det_name.c_str(), station_number, ('['+string(mat.name())+']').c_str(),
                x0, 0.0, z0, dist, dx, dz);
@@ -110,18 +132,14 @@ static Ref_t create_element(Detector& description, xml_h e, Ref_t sens)  {
     }
   }
 
-  if ( x_det.isSensitive() )   {
-    SensitiveDetector sd = sens;
-    xml_dim_t sd_typ = x_det.child(_U(sensitive));
-    env_vol.setSensitiveDetector(sens);
-    sd.setType(sd_typ.typeStr());
-  }
-
   Volume mother = description.pickMotherVolume(sdet);
-  pv = mother.placeVolume(env_vol,Transform3D(RotationZYX(rot.z(),rot.y(),rot.x()),Position(-pos.x(),-pos.y(),pos.z())));
+  Transform3D trafo(RotationZYX(rot.z(),rot.y(),rot.x()),Position(-pos.x(),-pos.y(),pos.z()));
+  Direction dir = trafo*Direction(0,0,1.);
+  pv = mother.placeVolume(env_vol,trafo);
   if ( x_det.hasAttr(_U(id)) )  {
     pv.addPhysVolID("system",x_det.id());
   }
+  printout(INFO,det_name,"+++ Need to shot in direction: (x,y,z) = (%g,%g,%g)",dir.X(),dir.Y(),dir.Z());
   sdet.setPlacement(pv);
   return sdet;
 }
