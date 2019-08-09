905e73162cf7fb3def69839513d777ab2f32f576
Andre Sailer <andre.philippe.sailer@cern.ch>
2019-08-09 13:31:19 +0200

    Try to compile against LCG_95 and LCG_96

--- .dd4hep-ci.d/compile_and_test.sh
+++ .dd4hep-ci.d/compile_and_test.sh
@@ -5,7 +5,7 @@ source /DD4hep/.dd4hep-ci.d/init_x86_64.sh
 cd /DD4hep
 mkdir build
 cd build
-cmake -GNinja -DDD4HEP_USE_GEANT4=ON -DBoost_NO_BOOST_CMAKE=ON -DDD4HEP_USE_LCIO=ON -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always -Werror" -DCMAKE_CXX_STANDARD=17 .. && \
+cmake -GNinja -DDD4HEP_USE_GEANT4=ON -DBoost_NO_BOOST_CMAKE=ON -DDD4HEP_USE_LCIO=ON -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-fdiagnostics-color=always -Werror" -DCMAKE_CXX_STANDARD=${STANDARD} .. && \
 ninja && \
 ninja install && \
 . ../bin/thisdd4hep.sh && \
--- .dd4hep-ci.d/init_x86_64.sh
+++ .dd4hep-ci.d/init_x86_64.sh
@@ -50,91 +50,15 @@ fi
 LCGREPO=/cvmfs/sft.cern.ch/lcg/releases/${LCG_RELEASE}
 BUILD_FLAVOUR=x86_64-${OS}-${COMPILER_VERSION}-${BUILD_TYPE}
 
-export LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/views/${LCG_RELEASE}/${BUILD_FLAVOUR}/lib64:/cvmfs/sft.cern.ch/lcg/views/${LCG_RELEASE}/${BUILD_FLAVOUR}/lib:$LD_LIBRARY_PATH
-
-#--------------------------------------------------------------------------------
-#     Compiler
-#--------------------------------------------------------------------------------
-
-if [[ ${COMPILER_TYPE} == "gcc" ]]; then
-    source /cvmfs/sft.cern.ch/lcg/releases/gcc/8.2.0-3fa06/x86_64-${OS}/setup.sh
-fi
-if [[ ${COMPILER_TYPE} == "clang" ]]; then
-    source /cvmfs/sft.cern.ch/lcg/releases/clang/8.0.0-ed577/x86_64-${OS}/setup.sh
-fi
-
-#--------------------------------------------------------------------------------
-#     CMake
-#--------------------------------------------------------------------------------
-
-export CMAKE_HOME=${LCGREPO}/CMake/3.14.3/${BUILD_FLAVOUR}
-export PATH=${CMAKE_HOME}/bin:$PATH
-
-#--------------------------------------------------------------------------------
-#     Python
-#--------------------------------------------------------------------------------
-if [[ $LCG_RELEASE =~ "python3" ]]; then
-    export PYTHONDIR=${LCGREPO}/Python/3.6.5/${BUILD_FLAVOUR}
-else
-    export PYTHONDIR=${LCGREPO}/Python/2.7.16/${BUILD_FLAVOUR}
-fi
-export PATH=${PYTHONDIR}/bin:$PATH
-export LD_LIBRARY_PATH=${PYTHONDIR}/lib:${LD_LIBRARY_PATH}
-
-#--------------------------------------------------------------------------------
-#     ROOT
-#--------------------------------------------------------------------------------
-
-export ROOTSYS=${LCGREPO}/ROOT/6.18.00/${BUILD_FLAVOUR}
-export ROOT_ROOT=$ROOTSYS
-export PYTHONPATH="$ROOTSYS/lib:$PYTHONPATH"
-export PATH="$ROOTSYS/bin:$PATH"
-export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
-
-#--------------------------------------------------------------------------------
-#     XercesC
-#--------------------------------------------------------------------------------
-
-export XercesC_ROOT=${LCGREPO}/XercesC/3.1.3/${BUILD_FLAVOUR}
-export PATH="$XercesC_ROOT/bin:$PATH"
-export LD_LIBRARY_PATH="$XercesC_ROOT/lib:$LD_LIBRARY_PATH"
-
-#--------------------------------------------------------------------------------
-#     Geant4
-#--------------------------------------------------------------------------------
-
-export Geant4_ROOT=${LCGREPO}/Geant4/10.05.p01/${BUILD_FLAVOUR}
-export G4LIB=$Geant4_ROOT/lib/Geant4-10.5.1/
-export G4ENV_INIT="${Geant4_ROOT}/bin/geant4.sh"
-export G4SYSTEM="Linux-g++"
+echo /cvmfs/sft.cern.ch/lcg/views/${LCG_RELEASE}/${BUILD_FLAVOUR}/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/${LCG_RELEASE}/${BUILD_FLAVOUR}/setup.sh
 
 
 #--------------------------------------------------------------------------------
 #     LCIO
 #--------------------------------------------------------------------------------
 export LCIO=/cvmfs/clicdp.cern.ch/software/LCIO/2.12.1/x86_64-${OS}-${COMPILER_VERSION}-LCG_96
+export LCIO_ROOT=${LCIO}
 export PYTHONPATH=${LCIO}/python
 export PATH=${LCIO}/bin:$PATH
 export LD_LIBRARY_PATH="$LCIO/lib:$LD_LIBRARY_PATH"
-
-
-#--------------------------------------------------------------------------------
-#     Boost
-#--------------------------------------------------------------------------------
-
-export BOOST_ROOT=${LCGREPO}/Boost/1.70.0/${BUILD_FLAVOUR}
-export LD_LIBRARY_PATH="${BOOST_ROOT}/lib:$LD_LIBRARY_PATH"
-
-#--------------------------------------------------------------------------------
-#     Ninja
-#--------------------------------------------------------------------------------
-
-export Ninja_HOME=${LCGREPO}/ninja/1.9.0/${BUILD_FLAVOUR}
-export PATH="$Ninja_HOME/bin:$PATH"
-
-#--------------------------------------------------------------------------------
-#     Doxygen
-#--------------------------------------------------------------------------------
-
-export Doxygen_HOME=${LCGREPO}/doxygen/1.8.15/${BUILD_FLAVOUR}
-export PATH="$Doxygen_HOME/bin:$PATH"
