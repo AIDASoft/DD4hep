dd92a1321b3bdad3ba0bb2fee60ee5285d3ec2de
Ben Couturier <ben.couturier@cern.ch>
2019-10-09 16:43:36 +0200

    MakeGaudiMap: add lib folder parallel to listcomponents to LD_LIBRARY_PATH

--- cmake/MakeGaudiMap.cmake
+++ cmake/MakeGaudiMap.cmake
@@ -4,6 +4,7 @@ message(STATUS " *** Gaudi listcomponents: Generate map for ${libname} ..." )
   #   
   FIND_PROGRAM( Gaudi_listcomponents_CMD listcomponents PATHS ${genmap_install_dir}/../bin ${DD4hep_DIR}/bin
     NO_DEFAULT_PATH NO_CMAKE_PATH)
+  GET_FILENAME_COMPONENT(GAUDI_LISTCOMP_INSTALL ${Gaudi_listcomponents_CMD} DIRECTORY)
   ###MESSAGE( STATUS " *** MakeRootMap ${genmap_install_dir}/../bin $ENV{DD4hep_DIR}/bin ${DD4hep_DIR}" )
   MESSAGE( STATUS " *** MakeGaudiMap.cmake run command : ${Gaudi_listcomponents_CMD} -o ${rootmapfile} ${libname}
                  WORKING_DIRECTORY ${genmap_install_dir} "
@@ -12,7 +13,7 @@ message(STATUS " *** Gaudi listcomponents: Generate map for ${libname} ..." )
     SET ( ENV{DYLD_LIBRARY_PATH} ${genmap_install_dir}:$ENV{DYLD_LIBRARY_PATH}:$ENV{DD4HEP_LIBRARY_PATH} )
     ##EXECUTE_PROCESS( COMMAND echo DYLD_LIBRARY_PATH = $ENV{DYLD_LIBRARY_PATH} )
   else()
-    SET ( ENV{LD_LIBRARY_PATH} ${genmap_install_dir}:$ENV{LD_LIBRARY_PATH} )
+    SET ( ENV{LD_LIBRARY_PATH} ${GAUDI_LISTCOMP_INSTALL}/../lib:${genmap_install_dir}:$ENV{LD_LIBRARY_PATH} )
   endif()
   # EXECUTE_PROCESS( COMMAND echo LD_LIBRARY_PATH = $ENV{LD_LIBRARY_PATH} )
   EXECUTE_PROCESS( COMMAND 
