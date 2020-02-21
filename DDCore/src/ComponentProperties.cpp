59e268e4d9bcc49e7d815e9fa26c31fb648288c8
Marko Petric <marko.petric@cern.ch>
2020-02-21 11:13:42 +0100

    Revert "Dropped useless macro -DD4HEP_DEFINE_PARSER_GRAMMAR_TYPE"
    This reverts commit 9a1e2611411d21feab093408d042303ecd38fc8b.

--- DDCore/src/ComponentProperties.cpp
+++ DDCore/src/ComponentProperties.cpp
@@ -250,6 +250,7 @@ namespace dd4hep {
 
 #include "DD4hep/detail/BasicGrammar_inl.h"
 #include "DD4hep/detail/ComponentProperties_inl.h"
+DD4HEP_DEFINE_PARSER_GRAMMAR_TYPE(Property)
 
 namespace dd4hep {
 
