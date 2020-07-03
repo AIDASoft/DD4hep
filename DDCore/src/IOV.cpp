bdd1bdb4af8177def9675e463cfce638119b76a6
Andre Sailer <andre.philippe.sailer@cern.ch>
2020-07-03 09:47:10 +0200

    IOV: declare static const members, as required by c++14

--- DDCore/src/IOV.cpp
+++ DDCore/src/IOV.cpp
@@ -24,6 +24,11 @@
 using namespace std;
 using namespace dd4hep;
 
+#if __cplusplus == 201402
+const IOV::Key_value_type IOV::MIN_KEY;
+const IOV::Key_value_type IOV::MAX_KEY;
+#endif
+
 #if 0
 /// Assignment operator
 IOVType& IOVType::operator=(const IOVType& copy)  {
