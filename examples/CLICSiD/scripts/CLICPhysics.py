4228db4b4c27e193309efb46dc34e50e6e7d6ec2
Markus FRANK <Markus.Frank@cern.ch>
2020-11-18 18:47:20 +0100

    Add copyright notice at head of files

--- examples/CLICSiD/scripts/CLICPhysics.py
+++ examples/CLICSiD/scripts/CLICPhysics.py
@@ -1,3 +1,14 @@
+# ==========================================================================
+#  AIDA Detector description implementation
+# --------------------------------------------------------------------------
+# Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
+# All rights reserved.
+#
+# For the licensing terms see $DD4hepINSTALL/LICENSE.
+# For the list of contributors see $DD4hepINSTALL/doc/CREDITS.
+#
+# ==========================================================================
+#
 """
    Subtest using CLICSid showing how to setup the Geant4 physics list
 
