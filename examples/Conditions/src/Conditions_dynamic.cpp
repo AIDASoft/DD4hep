1f51d8a0028fa518298f38fee6f98218760ace77
Markus FRANK <Markus.Frank@cern.ch>
2021-01-29 17:41:28 +0100

    Move Grammar headers to public include directory

--- examples/Conditions/src/Conditions_dynamic.cpp
+++ examples/Conditions/src/Conditions_dynamic.cpp
@@ -26,6 +26,18 @@
 
 namespace  {
   
+
+  class base_0   {
+  public:
+    int data_0 {0};
+    base_0() = default;
+    base_0(base_0&& copy) = default;
+    base_0(const base_0& copy) = default;
+    virtual ~base_0() = default;
+    base_0& operator=(base_0&& copy) = default;
+    base_0& operator=(const base_0& copy) = default;
+  };
+  
   template <typename T> class base_1   {
   public:
     T data_1;
@@ -36,7 +48,7 @@ namespace  {
     base_1& operator=(base_1&& copy) = default;
     base_1& operator=(const base_1& copy) = default;
   };
-  template <typename T> class base_2   {
+  template <typename T> class base_2: virtual public base_0   {
   public:
     T data_2;
     base_2() = default;
@@ -46,6 +58,7 @@ namespace  {
     base_2& operator=(base_2&& copy) = default;
     base_2& operator=(const base_2& copy) = default;
   };
+  
   class payload : public base_1<int>, public base_2<double>  {
   public:
     payload() = default;
@@ -75,25 +88,42 @@ using namespace dd4hep::ConditionExamples;
  *  \date    01/12/2016
  */
 static int condition_example (Detector& /* description */, int /* argc */, char** /* argv */)  {
+#define DATA_0  12345
+#define DATA_1  54321
+#define DATA_2  2.2222e2
+
   Condition cond("Cond","payload");
   payload& p = cond.bind<payload>();
-  p.data_1 = 1;
-  p.data_2 = 2e1;
 
+  p.data_0 = DATA_0;
+  p.data_1 = DATA_1;
+  p.data_2 = DATA_2;
   {
-    const base_1<int>* b1 = &p;
-    const base_2<double>* b2 = &p;
-    cout << "Payload: " << (void*)&p << " b1: " << (void*)b1 << "  b2: " << (void*)b2 << endl;
+    payload* pl = &p;
+    base_0*  b0 = pl;
+    base_1<int>* b1 = pl;
+    base_2<double>* b2 = pl;
+    cout << "Payload: " << (void*)pl
+	 << " b0: " << (void*)b0
+	 << " b1: " << (void*)b1
+	 << " b2: " << (void*)b2
+	 << endl;
   }
-
   const auto& pl = cond.as<payload>();
   cout << "Payload: " << pl.data_1 << "  " << pl.data_2 << endl;
+  if ( pl.data_0 != DATA_0 || pl.data_1 != DATA_1 || pl.data_2 != DATA_2 ) cout << "Test FAILED" << endl;
+
+  const auto& b0 = cond.as<base_0>();
+  cout << "Payload(base_0): " << (void*)&b0 << "  data: " << b0.data_0 << endl;
+  if ( b0.data_0 != DATA_0 ) cout << "Test FAILED" << endl;
 
   const auto& b1 = cond.as<base_1<int> >();
   cout << "Payload(base_1): " << (void*)&b1 << "  data: " << b1.data_1 << endl;
+  if ( b1.data_1 != DATA_1 ) cout << "Test FAILED" << endl;
 
   const auto& b2 = cond.as<base_2<double> >();
   cout << "Payload(base_2): " << (void*)&b2 << "  data: " << b2.data_2 << endl;
+  if ( b2.data_2 != DATA_2 ) cout << "Test FAILED" << endl;
 
   delete cond.ptr();
   cout << "Test PASSED" << endl;
