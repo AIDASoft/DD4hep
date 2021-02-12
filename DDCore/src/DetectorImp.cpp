ee2911688f7ed5141b7381d619eeaacf8f435c57
Andre Sailer <andre.philippe.sailer@cern.ch>
2021-02-12 16:13:31 +0100

    Switching units possiblein 6.22.8

--- DDCore/src/DetectorImp.cpp
+++ DDCore/src/DetectorImp.cpp
@@ -167,10 +167,10 @@ DetectorImp::DetectorImp(const string& name)
   static bool first = true;
   if ( first )    {
     first = false;
-#if defined(DD4HEP_USE_GEANT4_UNITS) && ROOT_VERSION_CODE >= ROOT_VERSION(6,23,0)
+#if defined(DD4HEP_USE_GEANT4_UNITS) && ROOT_VERSION_CODE >= ROOT_VERSION(6,22,7)
     printout(WARNING,"DD4hep","++ Using globally Geant4 unit system (mm,ns,MeV)");
     TGeoManager::SetDefaultUnits(TGeoManager::kG4Units);
-#elif ROOT_VERSION_CODE >= ROOT_VERSION(6,23,0)
+#elif ROOT_VERSION_CODE >= ROOT_VERSION(6,22,7)
     TGeoManager::SetDefaultUnits(TGeoManager::kRootUnits);
 #elif defined(DD4HEP_USE_GEANT4_UNITS) && ROOT_VERSION_CODE >= ROOT_VERSION(6,20,0)
     printout(WARNING,"DD4hep","++ Using globally Geant4 unit system (mm,ns,MeV)");
