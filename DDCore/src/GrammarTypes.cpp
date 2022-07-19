afb61f7b2cc6ce9d96f5cf320b27b0755195bde4
Markus Frank <Markus.Frank@cern.ch>
2022-07-19 16:27:15 +0200

    Resync with dd4hep master

--- DDCore/src/GrammarTypes.cpp
+++ DDCore/src/GrammarTypes.cpp
@@ -26,7 +26,9 @@
 #include <Math/Vector3D.h>
 
 namespace ROOT {
+
   namespace Math {
+
     /// Allow point insertion of a point in maps
     bool operator<(const XYZPoint& a, const XYZPoint& b);
     /// Allow 3-vector insertion of a  in maps
@@ -35,6 +37,7 @@ namespace ROOT {
     bool operator<(const PxPyPzEVector& a, const PxPyPzEVector& b);
   }
 }
+
 DD4HEP_DEFINE_PARSER_GRAMMAR_ROOTMATH(ROOT::Math::XYZPoint,eval_obj)
 DD4HEP_DEFINE_PARSER_GRAMMAR_ROOTMATH(ROOT::Math::XYZVector,eval_obj)
 DD4HEP_DEFINE_PARSER_GRAMMAR_ROOTMATH(ROOT::Math::PxPyPzEVector,eval_obj)
