6223bef9e5d7a1c5311a063c509f61af139e49aa
Markus Frank <Markus.Frank@cern.ch>
2022-11-25 20:15:45 +0100

    Extend MiniTel driver for usage as DDDigi data generator

--- examples/ClientTests/scripts/MiniTelSetup.py
+++ examples/ClientTests/scripts/MiniTelSetup.py
@@ -23,44 +23,20 @@ import DDG4TestSetup
 
 
 class Setup(DDG4TestSetup.Setup):
-  def __init__(self):
+  def __init__(self, geometry="/examples/ClientTests/compact/MiniTel.xml", macro=None, vis=None):
     install_dir = os.environ['DD4hepExamplesINSTALL']
-    DDG4TestSetup.Setup.__init__(self, "file:" + install_dir + "/examples/ClientTests/compact/MiniTel.xml")
+    DDG4TestSetup.Setup.__init__(self, "file:" + install_dir + geometry, macro=macro, vis=vis)
 
   def configure(self, output_level=None):
+    from dd4hep import DetElement
     DDG4TestSetup.Setup.configure(self)
-    # Now the calorimeters
-    seq, act = self.geant4.setupTracker('MyLHCBdetector1')
-    if output_level:
-      act.OutputLevel = output_level
-    seq, act = self.geant4.setupTracker('MyLHCBdetector2')
-    if output_level:
-      act.OutputLevel = output_level
-    seq, act = self.geant4.setupTracker('MyLHCBdetector3')
-    if output_level:
-      act.OutputLevel = output_level
-    seq, act = self.geant4.setupTracker('MyLHCBdetector4')
-    if output_level:
-      act.OutputLevel = output_level
-    # act.OutputLevel = 4
-    seq, act = self.geant4.setupTracker('MyLHCBdetector5')
-    if output_level:
-      act.OutputLevel = output_level
-    seq, act = self.geant4.setupTracker('MyLHCBdetector6')
-    if output_level:
-      act.OutputLevel = output_level
-    seq, act = self.geant4.setupTracker('MyLHCBdetector7')
-    if output_level:
-      act.OutputLevel = output_level
-    seq, act = self.geant4.setupTracker('MyLHCBdetector8')
-    if output_level:
-      act.OutputLevel = output_level
-    seq, act = self.geant4.setupTracker('MyLHCBdetector9')
-    if output_level:
-      act.OutputLevel = output_level
-    seq, act = self.geant4.setupTracker('MyLHCBdetector10')
-    if output_level:
-      act.OutputLevel = output_level
+    for i in self.geant4.description.detectors():
+      det = DetElement(i.second.ptr())
+      sd = self.description.sensitiveDetector(str(det.name()))
+      if sd.isValid():
+        seq, act = self.geant4.setupTracker(det.name())
+        if output_level:
+          act.OutputLevel = output_level
     return self
 
   def defineOutput(self, output='MiniTel_' + time.strftime('%Y-%m-%d_%H-%M')):
---
+++ examples/DDDigi/scripts/MiniTelGenerate.py
@@ -0,0 +1,95 @@
+# ==========================================================================
+#  AIDA Detector description implementation
+# --------------------------------------------------------------------------
+# Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
+# All rights reserved.
+#
+# For the licensing terms see $DD4hepINSTALL/LICENSE.
+# For the list of contributors see $DD4hepINSTALL/doc/CREDITS.
+#
+# ==========================================================================
+from __future__ import absolute_import, unicode_literals
+import sys
+import DDG4
+#
+"""
+
+   dd4hep example setup using the python configuration
+
+   \author  M.Frank
+   \version 1.0
+
+"""
+
+
+def run():
+  from g4units import GeV
+  from MiniTelSetup import Setup
+  args = DDG4.CommandLine()
+  m = Setup(geometry="/examples/ClientTests/compact/MiniTel_ext.xml")
+  kernel = m.kernel
+  if args.batch:
+    run = '/run/beamOn ' + str(args.events)
+    DDG4.setPrintLevel(DDG4.OutputLevel.WARNING)
+    cmds = []
+    if not args.runs:
+      args.runs = 1
+    for i in range(int(args.runs)):
+      cmds.append(run)
+    cmds.append('/ddg4/UI/terminate')
+    m.ui.Commands = cmds
+  m.configure()
+  wr = m.defineOutput(output='MiniTel')
+  wr.FilesByRun = True
+  gen = DDG4.GeneratorAction(kernel, "Geant4GeneratorActionInit/GenerationInit")
+  kernel.generatorAction().adopt(gen)
+
+  gun = DDG4.GeneratorAction(kernel, "Geant4IsotropeGenerator/IsotropPi+")
+  gun.Mask = 1
+  gun.Particle = 'pi+'
+  gun.Energy = 100 * GeV
+  gun.Multiplicity = 1
+  gun.Distribution = 'cos(theta)'
+  kernel.generatorAction().adopt(gun)
+
+  gun = DDG4.GeneratorAction(kernel, "Geant4IsotropeGenerator/IsotropPi-")
+  gun.Mask = 2
+  gun.Particle = 'pi-'
+  gun.Energy = 100 * GeV
+  gun.Multiplicity = 1
+  gun.Distribution = 'cos(theta)'
+  kernel.generatorAction().adopt(gun)
+
+  gun = DDG4.GeneratorAction(kernel, "Geant4IsotropeGenerator/IsotropE+1")
+  gun.Mask = 4
+  gun.Particle = 'e+'
+  gun.Energy = 50 * GeV
+  gun.Multiplicity = 1
+  gun.Distribution = 'cos(theta)'
+  kernel.generatorAction().adopt(gun)
+
+  gun = DDG4.GeneratorAction(kernel, "Geant4IsotropeGenerator/IsotropE+2")
+  gun.Mask = 8
+  gun.Particle = 'e+'
+  gun.Energy = 100 * GeV
+  gun.Multiplicity = 1
+  gun.Distribution = 'cos(theta)'
+  kernel.generatorAction().adopt(gun)
+  gun = None
+
+  gen = DDG4.GeneratorAction(kernel, "Geant4InteractionMerger/InteractionMerger")
+  gen.enableUI()
+  kernel.generatorAction().adopt(gen)
+
+  gen = DDG4.GeneratorAction(kernel, "Geant4PrimaryHandler/PrimaryHandler")
+  gen.enableUI()
+  kernel.generatorAction().adopt(gen)
+  gen = None
+
+  m.setupGenerator()
+  m.setupPhysics()
+  m.run()
+
+
+if __name__ == "__main__":
+  run()
