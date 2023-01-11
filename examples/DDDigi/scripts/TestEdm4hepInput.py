c5b5a4a27e6483a3785ca68e0e825899aafa318e
Markus Frank <Markus.Frank@cern.ch>
2023-01-11 17:45:51 +0100

    Fix python style errors

--- examples/DDDigi/scripts/TestEdm4hepInput.py
+++ examples/DDDigi/scripts/TestEdm4hepInput.py
@@ -14,7 +14,9 @@ from __future__ import absolute_import
 def run():
   import DigiTest
   digi = DigiTest.Test(geometry=None)
-  read = digi.input_action('DigiEdm4hepInput/SignalReader', mask=0x0, input=['../ClientTests/MiniTel_ddg4_edm4hep.root'])
+  read = digi.input_action('DigiEdm4hepInput/SignalReader',
+                           mask=0x0,
+                           input=['../ClientTests/MiniTel_ddg4_edm4hep.root'])
   dump = digi.event_action('DigiStoreDump/StoreDump', parallel=False)
   digi.check_creation([read, dump])
   digi.run_checked(num_events=5, num_threads=5, parallel=3)
