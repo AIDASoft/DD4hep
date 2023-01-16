da15d446dfa997957ac18cdadc08bb194708bf56
Markus Frank <Markus.Frank@cern.ch>
2023-01-16 14:54:41 +0100

    Make DDDigi I/O interface more common and add edm4hep write/read examples

--- DDDigi/src/DigiInputAction.cpp
+++ DDDigi/src/DigiInputAction.cpp
@@ -26,16 +26,43 @@ using namespace dd4hep::digi;
 DigiInputAction::DigiInputAction(const DigiKernel& kernel, const string& nam)
   : DigiEventAction(kernel, nam)
 {
-  declareProperty("input",   m_input_sources);
-  declareProperty("segment", m_input_segment);
-  declareProperty("mask",    m_input_mask);
-  declareProperty("rescan",  m_input_rescan);
+  declareProperty("input",            m_input_sources);
+  declareProperty("segment",          m_input_segment);
+  declareProperty("mask",             m_input_mask);
+  declareProperty("rescan",           m_input_rescan);
+  declareProperty("input_section",    m_input_section);
+  declareProperty("objects_enabled",  m_objects_enabled);
+  declareProperty("objects_disabled", m_objects_disabled);
   InstanceCount::increment(this);
 }
 
 /// Default destructor
 DigiInputAction::~DigiInputAction()   {
   InstanceCount::decrement(this);
+}
+
+/// Check if a event object should be loaded: Default YES unless inhibited by selection or veto
+bool DigiInputAction::object_loading_is_enabled(const std::string& nam)  const   {
+  /// If there are no required branches, we convert everything
+  if ( m_objects_enabled.empty() && m_objects_disabled.empty() )    {
+    return true;
+  }
+  /// Check for disabled collections:
+  for( const auto& bname : m_objects_disabled )    {
+    if ( bname == nam )
+      return false;
+  }
+  /// No selection to be performed: take them all
+  if ( m_objects_enabled.empty() )    {
+    return true;
+  }
+  /// .... Otherwise only the entities asked for
+  for( const auto& bname : m_objects_enabled )    {
+    if ( bname == nam )   {
+      return true;
+    }
+  }
+  return false;
 }
 
 /// Pre-track action callback
