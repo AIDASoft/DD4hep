cc29e9c92e8194c284615e0e67b909d5101e0a42
Markus Frank <Markus.Frank@cern.ch>
2023-03-15 17:08:49 +0100

    Remove ugly casts in the magnetic/electric field descriptors

--- DDCore/src/Fields.cpp
+++ DDCore/src/Fields.cpp
@@ -12,6 +12,7 @@
 //==========================================================================
 
 #include "DD4hep/Fields.h"
+#include "DD4hep/Printout.h"
 #include "DD4hep/InstanceCount.h"
 #include "DD4hep/detail/Handle.inl"
 
@@ -25,14 +26,14 @@ typedef OverlayedField::Object OverlayedFieldObject;
 DD4HEP_INSTANTIATE_HANDLE(OverlayedFieldObject);
 
 namespace {
-  void calculate_combined_field(vector<CartesianField>& v, const double* pos, double* field) {
+  void calculate_combined_field(vector<CartesianField>& v, const Position& pos, double* field) {
     for (const auto& i : v ) i.value(pos, field);
   }
 }
 
 /// Default constructor
-CartesianField::Object::Object()
-  : NamedObject(), type(UNKNOWN) {
+CartesianField::Object::Object() : TypedObject()  {
+  field_type = UNKNOWN;
   InstanceCount::increment(this);
 }
 
@@ -58,22 +59,27 @@ CartesianField::Properties& CartesianField::properties() const {
 
 /// Returns the 3 field components (x, y, z).
 void CartesianField::value(const Position& pos, Direction& field) const  {
-  value(pos,(double*)&field);
+  double fld[3] = {0e0, 0e0, 0e0};
+  double position[3] = {pos.X(), pos.Y(), pos.Z()};
+  data<Object>()->fieldComponents(position, fld);
+  field = Direction(fld[0], fld[1], fld[2]);
 }
 
 /// Returns the 3 field components (x, y, z).
-void CartesianField::value(const Position& pos, double* val) const   {
-  value((double*)&pos,val);
+void CartesianField::value(const Position& pos, double* field) const   {
+  double position[3] = {pos.X(), pos.Y(), pos.Z()};
+  data<Object>()->fieldComponents(position, field);
 }
 
 /// Returns the 3 field components (x, y, z).
-void CartesianField::value(const double* pos, double* val) const {
-  data<Object>()->fieldComponents(pos, val);
+void CartesianField::value(const double* pos, double* field) const {
+  data<Object>()->fieldComponents(pos, field);
 }
 
 /// Default constructor
-OverlayedField::Object::Object()
-  : type(0), electric(), magnetic() {
+OverlayedField::Object::Object() : TypedObject(), electric(), magnetic()
+{
+  field_type = CartesianField::OVERLAY;
   InstanceCount::increment(this);
 }
 
@@ -83,9 +89,10 @@ OverlayedField::Object::~Object() {
 }
 
 /// Object constructor
-OverlayedField::OverlayedField(const string& nam)
-  : Ref_t() {
-  assign(new Object(), nam, "overlay_field");
+OverlayedField::OverlayedField(const string& nam) : Ref_t() {
+  auto* obj = new Object();
+  assign(obj, nam, "overlay_field");
+  obj->field_type = CartesianField::OVERLAY;
 }
 
 /// Access to properties container
@@ -95,7 +102,7 @@ OverlayedField::Properties& OverlayedField::properties() const {
 
 /// Does the field change the energy of charged particles?
 bool OverlayedField::changesEnergy() const {
-  int field = data<Object>()->type;
+  int field = data<Object>()->field_type;
   return CartesianField::ELECTRIC == (field & CartesianField::ELECTRIC);
 }
 
@@ -103,45 +110,60 @@ bool OverlayedField::changesEnergy() const {
 void OverlayedField::add(CartesianField field) {
   if (field.isValid()) {
     Object* o = data<Object>();
-    if (o) {
-      int typ = field.fieldType();
+    if ( o ) {
+      int  typ   = field.fieldType();
       bool isEle = field.ELECTRIC == (typ & field.ELECTRIC);
       bool isMag = field.MAGNETIC == (typ & field.MAGNETIC);
       if (isEle) {
         vector < CartesianField > &v = o->electric_components;
         v.emplace_back(field);
-        o->type |= field.ELECTRIC;
-        o->electric = v.size() == 1 ? field : CartesianField();
+        o->field_type |= field.ELECTRIC;
+        o->electric = (v.size() == 1) ? field : CartesianField();
       }
       if (isMag) {
         vector < CartesianField > &v = o->magnetic_components;
         v.emplace_back(field);
-        o->type |= field.MAGNETIC;
-        o->magnetic = v.size() == 1 ? field : CartesianField();
+        o->field_type |= field.MAGNETIC;
+        o->magnetic = (v.size() == 1) ? field : CartesianField();
       }
       if (isMag || isEle)
         return;
-      throw runtime_error("OverlayedField::add: Attempt to add an unknown field type.");
+      except("OverlayedField","add: Attempt to add an unknown field type.");
     }
-    throw runtime_error("OverlayedField::add: Attempt to add to an invalid object.");
+    except("OverlayedField","add: Attempt to add an invalid object.");
   }
-  throw runtime_error("OverlayedField::add: Attempt to add an invalid field.");
+  except("OverlayedField","add: Attempt to add an invalid field.");
+}
+
+/// Returns the 3  magnetic field components (x, y, z).
+void OverlayedField::magneticField(const Position& pos, double* field) const   {
+  if ( isValid() )   {
+    field[0] = field[1] = field[2] = 0.0;
+    auto* obj = data<Object>();
+    CartesianField f = obj->magnetic;
+    if ( f.isValid() )
+      f.value(pos, field);
+    else
+      calculate_combined_field(obj->magnetic_components, pos, field);
+    return;
+  }
+  except("OverlayedField","add: Attempt to add an invalid field.");
 }
 
 /// Returns the 3 electric field components (x, y, z).
-void OverlayedField::combinedElectric(const double* pos, double* field) const {
+void OverlayedField::combinedElectric(const Position& pos, double* field) const {
   field[0] = field[1] = field[2] = 0.;
   calculate_combined_field(data<Object>()->electric_components, pos, field);
 }
 
 /// Returns the 3  magnetic field components (x, y, z).
-void OverlayedField::combinedMagnetic(const double* pos, double* field) const {
+void OverlayedField::combinedMagnetic(const Position& pos, double* field) const {
   field[0] = field[1] = field[2] = 0.;
   calculate_combined_field(data<Object>()->magnetic_components, pos, field);
 }
 
 /// Returns the 3 electric (val[0]-val[2]) and magnetic field components (val[3]-val[5]).
-void OverlayedField::electromagneticField(const double* pos, double* field) const {
+void OverlayedField::electromagneticField(const Position& pos, double* field) const {
   Object* o = data<Object>();
   field[0] = field[1] = field[2] = 0.;
   calculate_combined_field(o->electric_components, pos, field);
