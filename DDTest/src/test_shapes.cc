30cc428012a2a4b8e27b590e95cec9e34b02427b
Wouter Deconinck <wdconinc@gmail.com>
2024-03-27 12:52:31 -0500

    unit test for Polycone::addZPlanes with size == 1

---
+++ DDTest/src/test_shapes.cc
@@ -0,0 +1,33 @@
+#include "DD4hep/DDTest.h"
+
+#include "DD4hep/Shapes.h"
+
+#include <exception>
+#include <iostream>
+#include <assert.h>
+#include <cmath>
+
+//=============================================================================
+int main(int /* argc */, char** /* argv */ ){
+
+  // this should be the first line in your test
+  dd4hep::DDTest test( "shapes" );
+    
+  try{
+    
+    // --------------------------------------------------------------------
+    // Polycone
+
+    // add single plane with addZPlanes
+    Polycone polycone(0, 2*M_PI, {0, 0}, {1, 1}, {0, 1});
+    polycone.addZPlanes({0}, {1}, {0.5});
+
+    // --------------------------------------------------------------------
+  }
+  catch( std::exception &e ){
+    test.log( e.what() );
+    test.error( "exception occurred" );
+  }
+  return 0;
+}
+//=============================================================================
