295696795b4581bb6453b98ace35a9bab5c6b002
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2024-07-30 15:01:13 +0200

    kill white space

--- DDRec/include/DDRec/SurfaceManager.h
+++ DDRec/include/DDRec/SurfaceManager.h
@@ -1,5 +1,5 @@
 //==========================================================================
-//  AIDA Detector description implementation 
+//  AIDA Detector description implementation
 //--------------------------------------------------------------------------
 // Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
 // All rights reserved.
@@ -21,13 +21,13 @@
 namespace dd4hep {
   namespace rec {
 
-    /// typedef for surface maps, keyed by the cellID 
+    /// typedef for surface maps, keyed by the cellID
     typedef std::multimap< unsigned long, ISurface*> SurfaceMap ;
 
-    /** Surface manager class that holds maps of surfaces for all known 
-     *  sensitive detector types and  individual sub detectors. 
+    /** Surface manager class that holds maps of surfaces for all known
+     *  sensitive detector types and  individual sub detectors.
      *  Maps can be retrieved via detector name.
-     * 
+     *
      * @author F.Gaede, DESY
      * @date May, 11 2015
      * @version $Id$
@@ -48,23 +48,23 @@ namespace dd4hep {
 #endif
       /// No copy constructor
       SurfaceManager(const SurfaceManager& copy) = delete;
-      
+
       /// Default destructor
       ~SurfaceManager();
 
       /// No assignment operator
       SurfaceManager& operator=(const SurfaceManager& copy) = delete;
-      
+
       /** Get the maps of all surfaces associated to the given detector or
        *  type of detectors, e.g. map("tracker") returns a map with all surfaces
        *  assigned to tracking detectors. Returns 0 if no map exists.
        */
       const SurfaceMap* map( const std::string name ) const ;
 
-      
+
       ///create a string with all available maps and their size (number of surfaces)
       std::string toString() const ;
-      
+
     protected :
 
 
--- DDRec/src/SurfaceManager.cpp
+++ DDRec/src/SurfaceManager.cpp
@@ -1,5 +1,5 @@
 //==========================================================================
-//  AIDA Detector description implementation 
+//  AIDA Detector description implementation
 //--------------------------------------------------------------------------
 // Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
 // All rights reserved.
@@ -19,11 +19,11 @@
 #include <sstream>
 
 namespace dd4hep {
-  
+
   using namespace detail ;
 
   namespace rec {
-    
+
 
     SurfaceManager::SurfaceManager(const Detector& theDetector){
 
@@ -34,12 +34,12 @@ namespace dd4hep {
 
       initialize(theDetector) ;
     }
-    
+
     SurfaceManager::~SurfaceManager(){
       // nothing to do
     }
-    
-    
+
+
     const SurfaceMap* SurfaceManager::map( const std::string name ) const {
 
       SurfaceMapsMap::const_iterator it = _map.find( name ) ;
@@ -53,34 +53,34 @@ namespace dd4hep {
     }
 
     void SurfaceManager::initialize(const Detector& description) {
-      
+
       const std::vector<std::string>& types = description.detectorTypes() ;
 
       for(unsigned i=0,N=types.size();i<N;++i){
 
-        const std::vector<DetElement>& dets = description.detectors( types[i] ) ;  
+        const std::vector<DetElement>& dets = description.detectors( types[i] ) ;
 
         for(unsigned j=0,M=dets.size();j<M;++j){
 
           std::string name = dets[j].name() ;
 
           SurfaceHelper surfH( dets[j] ) ;
-	  
+
           const SurfaceList& detSL = surfH.surfaceList() ;
-  
-          // add an empty map for this detector in case there are no surfaces attached 
+
+          // add an empty map for this detector in case there are no surfaces attached
           _map.emplace(name , SurfaceMap());
 
           for( SurfaceList::const_iterator it = detSL.begin() ; it != detSL.end() ; ++it ){
             ISurface* surf =  *it ;
-	    
+
             // enter surface into map for this detector
             _map[ name ].emplace(surf->id(), surf );
 
             // enter surface into map for detector type
             _map[ types[i] ].emplace(surf->id(), surf );
 
-            // enter surface into world map 
+            // enter surface into world map
             _map[ "world" ].emplace(surf->id(), surf );
 
           }
@@ -90,14 +90,14 @@ namespace dd4hep {
     }
 
     std::string SurfaceManager::toString() const {
-      
+
       std::stringstream sstr ;
-       
+
       sstr << "--------  SurfaceManager contains the following maps : --------- " << std::endl ;
- 
+
       for( SurfaceMapsMap::const_iterator mi = _map.begin() ; mi != _map.end() ; ++mi ) {
-	
-        sstr << "  key: " <<  mi->first << " \t number of surfaces : " << mi->second.size() << std::endl ; 
+
+        sstr << "  key: " <<  mi->first << " \t number of surfaces : " << mi->second.size() << std::endl ;
       }
       sstr << "---------------------------------------------------------------- " << std::endl ;
 
