94df2c68f5f13ac4c9d3c472d010ed2e6501f17d
Andre Sailer <andre.philippe.sailer@cern.ch>
2025-02-07 10:00:11 +0100

    g4GraphicalScan.py: add license info

--- DDG4/python/bin/g4GraphicalScan.py
+++ DDG4/python/bin/g4GraphicalScan.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+# ==========================================================================
+#  AIDA Detector description implementation
+# --------------------------------------------------------------------------
+# Copyright (C) Organisation europeenne pour la Recherche nucleaire (CERN)
+# All rights reserved.
+#
+# For the licensing terms see $DD4hepINSTALL/LICENSE.
+# For the list of contributors see $DD4hepINSTALL/doc/CREDITS.
+#
+# ==========================================================================
 """
 Make a 2d slice through a dd4hep detector model.
 Present results as a series of TH2F
