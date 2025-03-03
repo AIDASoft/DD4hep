8c821295b620f415149fc12654f823e9e20d292f
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2025-03-03 17:23:10 -0500

    Update ROOT_LIBRARY_PATH in thisdd4hep.sh

--- cmake/thisdd4hep.sh
+++ cmake/thisdd4hep.sh
@@ -109,6 +109,8 @@ dd4hep_add_library_path    ${THIS}/lib;
 dd4hep_add_path PYTHONPATH ${THIS}/@DD4HEP_PYTHON_INSTALL_DIR@;
 #----ROOT_INCLUDE_PATH--------------------------------------------------------
 dd4hep_add_path ROOT_INCLUDE_PATH ${THIS}/include;
+#----ROOT_LIBRARY_PATH--------------------------------------------------------
+dd4hep_add_path ROOT_LIBRARY_PATH ${THIS}/lib;
 #-----------------------------------------------------------------------------
 if [ @APPLE@ ];
 then
