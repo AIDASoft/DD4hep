68e560c826e119fc847cd1592b7e1db55ac45cb5
jmcarcell <jmcarcell@users.noreply.github.com>
2025-03-07 17:06:47 +0100

    Add a test checking the result of outerMaterial()

---
+++ examples/ClientTests/scripts/call_inner_outer_material.py
@@ -0,0 +1,25 @@
+import os
+import argparse
+import dd4hep
+import DDRec
+det = dd4hep.Detector.getInstance()
+install_dir = os.environ['DD4hepExamplesINSTALL']
+det.fromCompact(f'{install_dir}/examples/ClientTests/compact/InnerOuterMaterial.xml')
+
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--call-inner-material', action='store_true', default=False)
+
+args = argparser.parse_args()
+
+sm = det.extension[DDRec.SurfaceManager]()
+multimap = sm.map('world')
+d = {}
+for k, v in multimap:
+    if args.call_inner_material:
+        innermat = v.innerMaterial()
+    outermat = v.outerMaterial()
+    d[k] = outermat.name()
+
+with open(f'surfaces_{"bad" if args.call_inner_material else "good"}.txt', 'w') as f:
+    for k, v in d.items():
+        f.write(f'{k} {v}\n')
