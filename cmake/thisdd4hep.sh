66e599f2ecbd2ff2adc4282b85c1d3d8b0e10084
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2025-10-07 10:46:38 +0200

    Set the correct environment when CMAKE_INSTALL_LIBDIR is used

--- cmake/thisdd4hep.sh
+++ cmake/thisdd4hep.sh
@@ -105,7 +105,7 @@ fi;
 #----PATH---------------------------------------------------------------------
 dd4hep_add_path PATH       ${THIS}/bin;
 #----LIBRARY_PATH-------------------------------------------------------------
-dd4hep_add_library_path    ${THIS}/lib;
+dd4hep_add_library_path    ${THIS}/@CMAKE_INSTALL_LIBDIR@;
 #----PYTHONPATH---------------------------------------------------------------
 dd4hep_add_path PYTHONPATH ${THIS}/@DD4HEP_PYTHON_INSTALL_DIR@;
 #----ROOT_INCLUDE_PATH--------------------------------------------------------
