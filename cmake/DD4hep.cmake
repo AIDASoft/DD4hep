3bc8fb32795d9b2fc10ba07106db3e1a65257825
Seth R Johnson <johnsonsr@ornl.gov>
2026-01-06 08:21:00 -0500

    Do not use cache variables or ROOT cmake variable

--- cmake/DD4hep.cmake
+++ cmake/DD4hep.cmake
@@ -82,21 +82,10 @@ function(dd4hep_generate_rootmap library)
     set(ENV_VAR LD_LIBRARY_PATH)
   endif()
 
-  set(DD4HEP_LIBRARY_PATH "$ENV{${ENV_VAR}}"
-    CACHE STRING
-    "Set ${ENV_VAR} when adding plugins"
-  )
-  if("$ENV{ROOT_LIBRARY_PATH}")
-    # Override path to required ROOT libraries from the environment
-    set(_default_root_lib_path "$ENV{ROOT_LIBRARY_PATH}")
-  else()
-    # Use directory specified by `find_package(ROOT)`
-    set(_default_root_lib_path "${ROOT_LIBRARY_DIR}")
-  endif()
-  set(DD4HEP_ROOT_LIBRARY_PATH "${_default_root_lib_path}"
-    CACHE STRING
-    "Set ROOT_LIBRARY_PATH when adding plugins"
-  )
+  set(DD4HEP_LIBRARY_PATH "$ENV{${ENV_VAR}}")
+
+  # Pull ROOT library path from environment
+  set(DD4HEP_ROOT_LIBRARY_PATH "$ENV{ROOT_LIBRARY_PATH}")
 
   string(JOIN ":" _ld_path
     "$<TARGET_FILE_DIR:${library}>"
