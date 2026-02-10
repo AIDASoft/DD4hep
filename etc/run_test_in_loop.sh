8b7b7522c742b52328f3958ddc2c5e32333300aa
Markus Frank <Markus.Frank@cern.ch>
2026-02-10 17:31:23 +0100

    Replace TAB with spaces

--- etc/run_test_in_loop.sh
+++ etc/run_test_in_loop.sh
@@ -17,7 +17,7 @@ usage()  {
     echo "   --name     <name>        Supply test name";
     echo "   -l         <number>      Number of test loops";
     echo "   --loops    <number>      Number of test loops";
-    echo "   -p         <number>      Parallelism flag";
+    echo "   -j         <number>      Parallelism flag";
     echo "   --parallel <number>      Parallelism flag";
     echo "   -g         <dir>         Non default Geant4 data directory";
     echo "   --g4-data  <dir>         Non default Geant4 data directory";
@@ -26,7 +26,7 @@ usage()  {
     exit EINVAL;
 }
 
-PARSED_ARGUMENTS=$(getopt -a -n run_test_in_loop -o hg:Nl:R:p: --longoptions help,g4-data:,show-only,loops:,test-regex:,parallel: -- "[run_test_in_loop]" $*)
+PARSED_ARGUMENTS=$(getopt -a -n run_test_in_loop -o hg:Nl:R:j: --longoptions help,g4-data:,show-only,loops:,test-regex:,parallel: -- "[run_test_in_loop]" $*)
 VALID_ARGUMENTS=$?
 if [ "$VALID_ARGUMENTS" != "0" ]; then
     do_print "Invalid arguments to cmake step";
@@ -44,8 +44,8 @@ do
     case "${1}" in
 	-g | --g4-data)       g4_data_dir=${2};           shift 2 ;;
 	-l | --loops)         num_loop=${2};              shift 2 ;;
-	-R | --tests-regex)   test_name=${2};             shift 2 ;;
-	-p | --parallel)      parallel=${2};              shift 2 ;;
+	-R | --tests-regex)   test_name="-R ${2}";        shift 2 ;;
+	-j | --parallel)      parallel=${2};              shift 2 ;;
 	-N | --show-only)     noop="-N";                  shift ;;
 	-h | --help)    usage;                            shift ;;
 	# -- means the end of the arguments; drop this, and break out of the while loop
@@ -57,15 +57,12 @@ do
     esac;
 done;
 #
-if test -z "${test_name}"; then
-    usage;
-fi;
 if test -z "${num_loop}"; then
     usage;
 fi;
 #
-if test -z "${GEANT4_DATA_DIR}"; then
-    export GEANT4_DATA_DIR=/scratch/online/frankm/SW/Geant4Data;
+if test -z "${g4_data_dir}"; then
+    g4_data_dir=/scratch/online/frankm/SW/Geant4Data;
 fi;
 #
 #
@@ -94,7 +91,7 @@ line="==============================";
 for i in $(seq 1 ${num_loop}); do
     exec_command echo "${line}${line}${line}${line}${line}";
     exec_command echo "Executing test "${test_name}" in loop number ${i}";
-    exec_command ctest -j ${parallel} ${noop} -R ${test_name};
+    exec_command ctest -j ${parallel} ${noop} ${test_name};
     if test "$?" = "0"; then
 	exec_command echo "=====> Test ${i} SUCCEEDED: exit code: $? ";
     else
