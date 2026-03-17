d9ea5944a4d9969a1d4b2ecc0100638e90f1859d
Andre Sailer <andre.philippe.sailer@cern.ch>
2026-03-17 10:39:08 +0100

    EDM4hepInput: add eventHeaderCollectionName Parameter

--- DDG4/python/DDSim/Helper/EDM4hep.py
+++ DDG4/python/DDSim/Helper/EDM4hep.py
@@ -10,4 +10,15 @@ class EDM4hep(Input, MCParticleMixin):
   def __init__(self):
     super(EDM4hep, self).__init__()
     self._parameters["MCParticleCollectionName"] = "MCParticles"
+    self._parameters["EventHeaderCollectionName"] = "EventHeader"
     self._closeProperties()
+
+  @property
+  def eventHeaderCollectionName(self):
+    """Set the name of the collection containing the EventHeader input.
+    """
+    return self._parameters["EventHeaderCollectionName"]
+
+  @eventHeaderCollectionName.setter
+  def eventHeaderCollectionName(self, val):
+    self._parameters["MCParticleCollectionName"] = val
